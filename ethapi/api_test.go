b543e656b0c282f743c3f3053ed2a2bb3e7445a9
jenikd <jd@fantom.foundation>
2025-02-19 09:06:25 +0100

    Fix mock function call

--- ethapi/api_test.go
+++ ethapi/api_test.go
@@ -320,7 +320,7 @@ func setExpectedStateCalls(mockState *state.MockStateDB) {
 	mockState.EXPECT().GetCode(any).Return(nil).AnyTimes()
 	mockState.EXPECT().Witness().AnyTimes()
 	mockState.EXPECT().GetRefund().AnyTimes()
-	mockState.EXPECT().Finalise().AnyTimes()
+	mockState.EXPECT().EndTransaction().AnyTimes()
 	mockState.EXPECT().GetLogs(any, any).AnyTimes()
 	mockState.EXPECT().TxIndex().AnyTimes()
 }
