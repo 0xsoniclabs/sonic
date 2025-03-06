577edfde07199638b889e71cccf3f121e85be34c
facuMH <facumolina15@gmail.com>
2025-03-06 10:01:12 +0100

    fix rpcprovider documentation and test for committee certificates

--- scc/light_client/provider/rpc.go
+++ scc/light_client/provider/rpc.go
@@ -1,56 +1,88 @@
+// Package provider contains the implementation of the Provider interface
 package provider
 
 import (
 	"fmt"
+	"math"
 
+	"github.com/0xsoniclabs/sonic/ethapi"
 	"github.com/0xsoniclabs/sonic/scc"
 	"github.com/0xsoniclabs/sonic/scc/cert"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-// RPCProvider implements the Provider interface and adds methods for RPC calls.
+// RPCProvider implements the Provider interface and provides methods for
+// making RPC calls through an Ethereum client.
 type RPCProvider struct {
-	// Embed Provider interface to enbable decorator pattern
-	Provider
-
-	// client is the Ethereum client to use for RPC calls.
+	// client is the Ethereum client used for making RPC calls.
 	client *ethclient.Client
 }
 
-// NewRPCProvider creates a new RPCProvider with the given Ethereum client.
-// Returns nil if the client could not be created.
-func NewRPCProvider(provider Provider, client *ethclient.Client) *RPCProvider {
-	// TODO: get real client
-	// client, err := ethclient.Dial(fmt.Sprintf("http://localhost:%d", 0))
-	// if err != nil {
-	// 	return nil
-	// }
+// NewRPCProvider creates a new instance of RPCProvider with the given
+// Ethereum client. Returns nil if the client could not be created.
+// The resulting Provider must be closed after use.
+//
+// Parameters:
+// - client: The Ethereum client to use for RPC calls.
+//
+// Returns:
+// - *RPCProvider: A new instance of RPCProvider.
+func NewRPCProvider(client *ethclient.Client) *RPCProvider {
+	// TODO: get real URL and make my own client?
 	return &RPCProvider{
-		Provider: provider,
-		client:   client,
+		client: client,
 	}
 }
 
+// Close closes the RPCProvider and its underlying Ethereum client.
+func (rpcp *RPCProvider) Close() {
+	rpcp.client.Close()
+}
+
+// GetCommitteeCertificate retrieves committee certificates for a given period
+// and a maximum number of results.
+//
+// Parameters:
+// - first: The starting period for which to retrieve committee certificates.
+// - maxResults: The maximum number of committee certificates to retrieve.
+//
+// Returns:
+// - []cert.CommitteeCertificate: A slice of committee certificates.
+// - error: An error if the retrieval fails.
 func (rpcp *RPCProvider) GetCommitteeCertificate(first scc.Period, maxResults uint64) ([]cert.CommitteeCertificate, error) {
-	if rpcp.client == nil && rpcp.Provider == nil {
-		return nil, fmt.Errorf("Can not fetch certificates from provider nor network")
+	if rpcp.client == nil {
+		return nil, fmt.Errorf("No client available")
 	}
-	results := []cert.CommitteeCertificate{}
-	if rpcp.client != nil {
-		err := rpcp.client.Client().Call(&results, "sonic_getCommitteeCertificates", "0x0", "max")
-		if err != nil {
-			return nil, err
-		}
-		return results, nil
-	}
-	if rpcp.Provider != nil {
 
+	maxString := fmt.Sprintf("%x", maxResults)
+	if maxResults == math.MaxUint64 {
+		maxString = "max"
 	}
-	return nil, nil
+	results := []ethapi.CommitteeCertificateJson{}
+	err := rpcp.client.Client().Call(
+		&results, "sonic_getCommitteeCertificates",
+		fmt.Sprintf("%x", first),
+		maxString,
+	)
+	if err != nil {
+		return nil, err
+	}
+	certs := []cert.CommitteeCertificate{}
+	for _, res := range results {
+		certs = append(certs, res.ToCertificate())
+	}
+	return certs, nil
 }
 
-// GetBlockCertificate returns the block certificate for the given chain ID and block number.
+// GetBlockCertificate returns the block certificate for the given block number.
+//
+// Parameters:
+// - number: The block number for which to retrieve the block certificate.
+//
+// Returns:
+// - cert.BlockCertificate: The block certificate for the given block number.
+// - error: An error if the retrieval fails.
 func GetBlockCertificate(number idx.Block) (cert.BlockCertificate, error) {
 	return cert.BlockCertificate{}, nil
 }
