b8a115fca78e75e08d3b81901fb2b96224b8806f
Luis Ayuso <luis@ph3.at>
2025-03-07 09:42:05 +0100

    Add feature set enumeration for configuration of fake-nets

--- opera/rules.go
+++ opera/rules.go
@@ -2,6 +2,7 @@ package opera
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -428,6 +429,42 @@ func (r Rules) String() string {
 	return string(b)
 }
 
+// FeatureSet is an enumeration of different releases, each one enabling a
+// different set of features. These are an abstraction that allows to reason
+// about the different releases instead of isolated upgrades.
+// Feature sets are exclusive, and a (fake-)net cannot be configured with
+// more than one feature set at the time.
+type FeatureSet int
+
+const (
+	SonicFeatures   FeatureSet = iota // < enables the initial sonic release features
+	AllegroFeatures                   // < enables the allegro release features
+)
+
+func (fs FeatureSet) ToUpgrades() (Upgrades, error) {
+	var res Upgrades
+	switch fs {
+	case SonicFeatures:
+		res = GetSonicUpgrades()
+	case AllegroFeatures:
+		res = GetAllegroUpgrades()
+	default:
+		return res, fmt.Errorf("unknown feature set: %v", fs)
+	}
+	return res, nil
+}
+
+func (fs FeatureSet) String() string {
+	switch fs {
+	case SonicFeatures:
+		return "sonic"
+	case AllegroFeatures:
+		return "allegro"
+	default:
+		return "unknown"
+	}
+}
+
 func GetSonicUpgrades() Upgrades {
 	return Upgrades{
 		Berlin:  true,
@@ -444,6 +481,6 @@ func GetAllegroUpgrades() Upgrades {
 		London:  true,
 		Llr:     false,
 		Sonic:   true,
-		Allegro: false,
+		Allegro: true,
 	}
 }
