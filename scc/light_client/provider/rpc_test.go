c52820964748035e2c8377863c5b195aa6820513
facuMH <facumolina15@gmail.com>
2025-03-07 13:36:28 +0100

    Add RpcClient interface and mock. Add unit tests for RpcProvider. Change ethclient for eth/rpc.client

--- scc/light_client/provider/rpc_test.go
+++ scc/light_client/provider/rpc_test.go
@@ -1,51 +1,29 @@
 package provider
 
 import (
-	"context"
 	"fmt"
 	"math"
+	"math/big"
 	"testing"
 
+	"github.com/0xsoniclabs/sonic/ethapi"
+	"github.com/0xsoniclabs/sonic/scc"
+	"github.com/0xsoniclabs/sonic/scc/bls"
+	"github.com/0xsoniclabs/sonic/scc/cert"
 	"github.com/0xsoniclabs/sonic/tests"
 	"github.com/0xsoniclabs/sonic/tests/contracts/counter_event_emitter"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRPCProvider_GetCommitteeCertificates_CanRetrieveCertificates(t *testing.T) {
+func TestRpcProvider_ReportsErrorForNilClient(t *testing.T) {
 	require := require.New(t)
 
-	// start network
-	net, client := startNetAndGetClient(t)
-
-	providerFromClient := NewRPCProviderFromClient(client)
-	providerFromURL, err := NewRPCProviderFromURL(fmt.Sprintf("http://localhost:%d", net.GetPort()))
-	require.NoError(err)
-
-	for _, provider := range []*RPCProvider{providerFromClient, providerFromURL} {
-
-		// get certificates
-		certs, err := provider.GetCommitteeCertificates(0, math.MaxUint64)
-		require.NoError(err)
-		provider.Close()
-
-		chainId, err := client.ChainID(context.Background())
-		require.NoError(err)
-		for _, cert := range certs {
-			require.Equal(chainId.Uint64(), cert.Subject().ChainId)
-			require.NotEmpty(cert.Subject().Committee)
-			require.NotEmpty(cert.Subject().Committee.Members()[0].PublicKey)
-			require.NotEmpty(cert.Subject().Committee.Members()[0].ProofOfPossession)
-			require.NotZero(cert.Subject().Committee.Members()[0].VotingPower)
-		}
-	}
-}
-
-func TestRPCProvider_ReportsErrorForNilClient(t *testing.T) {
-	require := require.New(t)
-
-	provider := NewRPCProviderFromClient(nil)
+	provider := NewRpcProviderFromClient(nil)
 
 	// get committee certificates
 	_, err := provider.GetCommitteeCertificates(0, 1)
@@ -56,14 +34,220 @@ func TestRPCProvider_ReportsErrorForNilClient(t *testing.T) {
 	require.Error(err)
 }
 
-func TestRPCProvider_NewRPCProvider_ReportsErrorForInvalidURL(t *testing.T) {
+func TestRpcProvider_FailsToRequestAfterClose(t *testing.T) {
 	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
 
-	_, err := NewRPCProviderFromURL("not-a-url")
+	provider := NewRpcProviderFromClient(client)
+
+	// close provider
+	client.EXPECT().Close()
+	provider.Close()
+
+	// get committee certificates
+	_, err := provider.GetCommitteeCertificates(0, 1)
+	require.Error(err)
+
+	// get block certificates
+	_, err = provider.GetBlockCertificates(0, 1)
 	require.Error(err)
 }
 
-func TestRPCProvider_GetBlockCertificates_CanRetrieveCertificates(t *testing.T) {
+func TestRpcProvider_NewRpcProvider_ReportsErrorForInvalidURL(t *testing.T) {
+	require := require.New(t)
+
+	_, err := NewRpcProviderFromURL("not-a-url")
+	require.Error(err)
+}
+
+func TestRpcProvider_GetCertificates_PropagatesErrorFromClientCall(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
+
+	committeeError := fmt.Errorf("committee error")
+	client.EXPECT().Call(gomock.Any(), "sonic_getCommitteeCertificates",
+		gomock.Any(), gomock.Any()).Return(committeeError)
+
+	blockError := fmt.Errorf("block error")
+	client.EXPECT().Call(gomock.Any(), "sonic_getBlockCertificates",
+		gomock.Any(), gomock.Any()).Return(blockError)
+
+	provider := NewRpcProviderFromClient(client)
+
+	// get committee certificates
+	_, err := provider.GetCommitteeCertificates(0, 1)
+	require.ErrorIs(err, committeeError)
+
+	// get block certificates
+	_, err = provider.GetBlockCertificates(0, 1)
+	require.ErrorIs(err, blockError)
+}
+
+func TestRpcProvider_GetCommitteeCertificates_ReportsCorruptedCertificatesOutOfOrder(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
+	provider := NewRpcProviderFromClient(client)
+
+	tests := [][]ethapi.CommitteeCertificate{
+		{
+			makeCommitteeCertForPeriod(1),
+		},
+		{
+			makeCommitteeCertForPeriod(0),
+			makeCommitteeCertForPeriod(2),
+		},
+	}
+
+	for _, committees := range tests {
+
+		// client setup
+		client.EXPECT().Call(gomock.Any(), "sonic_getCommitteeCertificates",
+			gomock.Any(), gomock.Any()).
+			DoAndReturn(
+				func(result *[]ethapi.CommitteeCertificate, method string, args ...interface{}) error {
+					*result = committees
+					return nil
+				})
+
+		// get committee certificates
+		_, err := provider.GetCommitteeCertificates(0, 3)
+		require.ErrorContains(err, "out of order")
+	}
+}
+
+func TestRpcProvider_GetCommitteeCertificates_FailsIfMoreCertificatesThanRequestedAreReturned(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
+	provider := NewRpcProviderFromClient(client)
+
+	client.EXPECT().Call(gomock.Any(), "sonic_getCommitteeCertificates",
+		gomock.Any(), gomock.Any()).DoAndReturn(
+		func(result *[]ethapi.CommitteeCertificate, method string, args ...interface{}) error {
+			*result = []ethapi.CommitteeCertificate{
+				makeCommitteeCertForPeriod(0),
+				makeCommitteeCertForPeriod(1),
+			}
+			return nil
+		})
+
+	// get committee certificates
+	_, err := provider.GetCommitteeCertificates(0, 1)
+	require.ErrorContains(err, "Too many certificates")
+}
+
+func TestRpcProvider_GetBlockCertificates_ReportsCorruptedCertificatesOutOfOrder(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
+	provider := NewRpcProviderFromClient(client)
+
+	tests := [][]ethapi.BlockCertificate{
+		{
+			makeBlockCertForNumber(1),
+		},
+		{
+			makeBlockCertForNumber(0),
+			makeBlockCertForNumber(2),
+		},
+	}
+
+	for _, blocks := range tests {
+		client.EXPECT().Call(gomock.Any(), "sonic_getBlockCertificates",
+			gomock.Any(), gomock.Any()).DoAndReturn(
+			func(result *[]ethapi.BlockCertificate, method string, args ...interface{}) error {
+				*result = blocks
+				return nil
+			})
+
+		// get block certificates
+		_, err := provider.GetBlockCertificates(0, 3)
+		require.ErrorContains(err, "out of order")
+	}
+}
+
+func TestRpcProvider_GetBlockCertificates_FailsIfMoreCertificatesThanRequestedAreReturned(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	client := NewMockRpcClient(ctrl)
+	provider := NewRpcProviderFromClient(client)
+
+	client.EXPECT().Call(gomock.Any(), "sonic_getBlockCertificates",
+		gomock.Any(), gomock.Any()).DoAndReturn(
+		func(result *[]ethapi.BlockCertificate, method string, args ...interface{}) error {
+			*result = []ethapi.BlockCertificate{
+				makeBlockCertForNumber(0),
+				makeBlockCertForNumber(1),
+			}
+			return nil
+		})
+
+	// get block certificates
+	_, err := provider.GetBlockCertificates(0, 1)
+	require.ErrorContains(err, "Too many certificates")
+}
+
+/// helper functions
+
+func makeCommitteeCertForPeriod(period scc.Period) ethapi.CommitteeCertificate {
+	return ethapi.CommitteeCertificate{
+		ChainId:   1,
+		Period:    uint64(period),
+		Members:   []scc.Member{},
+		Signers:   cert.BitSet[scc.MemberId]{},
+		Signature: bls.Signature{},
+	}
+}
+
+func makeBlockCertForNumber(number idx.Block) ethapi.BlockCertificate {
+	return ethapi.BlockCertificate{
+		ChainId:   1,
+		Number:    uint64(number),
+		Hash:      [32]byte{},
+		StateRoot: [32]byte{},
+	}
+}
+
+////////////////////////////////////////
+// net tests
+////////////////////////////////////////
+
+func TestRpcProvider_GetCommitteeCertificates_CanRetrieveCertificates(t *testing.T) {
+	require := require.New(t)
+
+	// start network
+	net, client := startNetAndGetClient(t)
+
+	url := fmt.Sprintf("http://localhost:%d", net.GetPort())
+
+	providerFromClient := NewRpcProviderFromClient(client.Client())
+	providerFromURL, err := NewRpcProviderFromURL(url)
+	require.NoError(err)
+
+	chainId := getChainId(t, client.Client())
+
+	for _, provider := range []*RpcProvider{providerFromClient, providerFromURL} {
+
+		// get certificates
+		certs, err := provider.GetCommitteeCertificates(0, math.MaxUint64)
+		require.NoError(err)
+		provider.Close()
+
+		require.NotZero(len(certs))
+		for _, cert := range certs {
+			require.Equal(chainId.Uint64(), cert.Subject().ChainId)
+			require.NotEmpty(cert.Subject().Committee)
+			require.NotZero(cert.Subject().Committee.Members()[0].PublicKey)
+			require.NotZero(cert.Subject().Committee.Members()[0].ProofOfPossession)
+			require.NotZero(cert.Subject().Committee.Members()[0].VotingPower)
+		}
+	}
+}
+
+func TestRpcProvider_GetBlockCertificates_CanRetrieveCertificates(t *testing.T) {
 	require := require.New(t)
 
 	// start network
@@ -79,11 +263,15 @@ func TestRPCProvider_GetBlockCertificates_CanRetrieveCertificates(t *testing.T) 
 		require.NoError(err, "failed to increment counter")
 	}
 
-	providerFromClient := NewRPCProviderFromClient(client)
-	providerFromURL, err := NewRPCProviderFromURL(fmt.Sprintf("http://localhost:%d", net.GetPort()))
+	url := fmt.Sprintf("http://localhost:%d", net.GetPort())
+
+	providerFromClient := NewRpcProviderFromClient(client.Client())
+	providerFromURL, err := NewRpcProviderFromURL(url)
 	require.NoError(err)
 
-	for _, provider := range []*RPCProvider{providerFromClient, providerFromURL} {
+	chainId := getChainId(t, client.Client())
+
+	for _, provider := range []*RpcProvider{providerFromClient, providerFromURL} {
 
 		// get certificates
 		certs, err := provider.GetBlockCertificates(1, numBlocks-1)
@@ -94,8 +282,7 @@ func TestRPCProvider_GetBlockCertificates_CanRetrieveCertificates(t *testing.T) 
 		headers, err := net.GetHeaders()
 		require.NoError(err)
 
-		chainId, err := client.ChainID(context.Background())
-		require.NoError(err)
+		require.NotZero(len(certs))
 		for _, cert := range certs {
 			require.Equal(chainId.Uint64(), cert.Subject().ChainId)
 			if cert.Subject().Number >= idx.Block(len(headers)) {
@@ -109,26 +296,28 @@ func TestRPCProvider_GetBlockCertificates_CanRetrieveCertificates(t *testing.T) 
 	}
 }
 
-func TestRPCProvider_CanRequestMaxNumberOfResults(t *testing.T) {
+func TestRpcProvider_CanRequestMaxNumberOfResults(t *testing.T) {
 	require := require.New(t)
 
 	// start network
 	net, client := startNetAndGetClient(t)
 
-	providerFromClient := NewRPCProviderFromClient(client)
-	providerFromURL, err := NewRPCProviderFromURL(fmt.Sprintf("http://localhost:%d", net.GetPort()))
+	url := fmt.Sprintf("http://localhost:%d", net.GetPort())
+
+	providerFromClient := NewRpcProviderFromClient(client.Client())
+	providerFromURL, err := NewRpcProviderFromURL(url)
 	require.NoError(err)
 
-	for _, provider := range []*RPCProvider{providerFromClient, providerFromURL} {
+	for _, provider := range []*RpcProvider{providerFromClient, providerFromURL} {
 		comCerts, err := provider.GetCommitteeCertificates(0, math.MaxUint64)
 		require.NoError(err)
 		provider.Close()
-		require.NotZero(comCerts)
+		require.NotZero(len(comCerts))
 
 		blockCerts, err := provider.GetBlockCertificates(0, math.MaxUint64)
 		require.NoError(err)
 		provider.Close()
-		require.NotZero(blockCerts)
+		require.NotZero(len(blockCerts))
 	}
 }
 
@@ -145,4 +334,12 @@ func startNetAndGetClient(t *testing.T) (*tests.IntegrationTestNet, *ethclient.C
 	client, err := net.GetClient()
 	require.NoError(err)
 	return net, client
+}
+
+func getChainId(t *testing.T, client *rpc.Client) *big.Int {
+	t.Helper()
+	var result hexutil.Big
+	err := client.Call(&result, "eth_chainId")
+	require.NoError(t, err)
+	return result.ToInt()
 }
