92f7a9728417fcdb5bb987333ffadc89db48a935
facuMH <facumolina15@gmail.com>
2025-03-07 13:36:28 +0100

    Add RpcClient interface and mock. Add unit tests for RpcProvider. Change ethclient for eth/rpc.client

--- scc/light_client/provider/rpc.go
+++ scc/light_client/provider/rpc.go
@@ -7,32 +7,33 @@ import (
 	"github.com/0xsoniclabs/sonic/scc"
 	"github.com/0xsoniclabs/sonic/scc/cert"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// RPCProvider implements the Provider interface and provides methods
+// RpcProvider implements the Provider interface and provides methods
 // making RPC calls through an Ethereum client.
-type RPCProvider struct {
+type RpcProvider struct {
 	// client is the Ethereum client used for making RPC calls.
-	client *ethclient.Client
+	client RpcClient
 }
 
-// NewRPCProviderFromClient creates a new instance of RPCProvider with the given
-// Ethereum client.
+// NewRpcProviderFromClient creates a new instance of RpcProvider with the given
+// Ethereum client. The resulting Provider takes ownership of the client and
+// will close it when the Provider is closed.
 // The resulting Provider must be closed after use.
 //
 // Parameters:
 // - client: The Ethereum client to use for RPC calls.
 //
 // Returns:
-// - *RPCProvider: A new instance of RPCProvider.
-func NewRPCProviderFromClient(client *ethclient.Client) *RPCProvider {
-	return &RPCProvider{
+// - *RpcProvider: A new instance of RpcProvider.
+func NewRpcProviderFromClient(client RpcClient) *RpcProvider {
+	return &RpcProvider{
 		client: client,
 	}
 }
 
-// NewRPCProviderFromURL creates a new instance of RPCProvider with a new Ethereum client
+// NewRpcProviderFromURL creates a new instance of RpcProvider with a new Ethereum client
 // connected to the given URL.
 // The resulting Provider must be closed after use.
 //
@@ -40,19 +41,24 @@ func NewRPCProviderFromClient(client *ethclient.Client) *RPCProvider {
 // - url: The URL of the RPC node to connect to.
 //
 // Returns:
-// - *RPCProvider: A new instance of RPCProvider.
+// - *RpcProvider: A new instance of RpcProvider.
 // - error: An error if the connection fails.
-func NewRPCProviderFromURL(url string) (*RPCProvider, error) {
-	client, err := ethclient.Dial(url)
+func NewRpcProviderFromURL(url string) (*RpcProvider, error) {
+	client, err := rpc.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	return NewRPCProviderFromClient(client), nil
+	return NewRpcProviderFromClient(client), nil
 }
 
-// Close closes the RPCProvider and its underlying Ethereum client.
-func (rpcp *RPCProvider) Close() {
+// Close closes the RpcProvider and its underlying Ethereum client.
+func (rpcp *RpcProvider) Close() {
 	rpcp.client.Close()
+	rpcp.client = nil
+}
+
+func (rpcp RpcProvider) IsClosed() bool {
+	return rpcp.client == nil
 }
 
 // GetCommitteeCertificates returns up to `maxResults` consecutive committee
@@ -63,24 +69,34 @@ func (rpcp *RPCProvider) Close() {
 // - maxResults: The maximum number of committee certificates to retrieve.
 //
 // Returns:
-// - []cert.CommitteeCertificate: A slice of committee certificates.
-// - error: An error if the client is nil or the retrieval fails.
-func (rpcp RPCProvider) GetCommitteeCertificates(first scc.Period, maxResults uint64) ([]cert.CommitteeCertificate, error) {
-	if rpcp.client == nil {
+//   - []cert.CommitteeCertificate: A slice of committee certificates.
+//   - error: An error if the client is nil, the retrieval fails or the
+//     certificates are out of order or more than requested.
+func (rpcp RpcProvider) GetCommitteeCertificates(first scc.Period, maxResults uint64) ([]cert.CommitteeCertificate, error) {
+	if rpcp.IsClosed() {
 		return nil, fmt.Errorf("No client available")
 	}
 
-	results := []ethapi.CommitteeCertificateJson{}
-	err := rpcp.client.Client().Call(
-		&results, "sonic_getCommitteeCertificates",
+	results := []ethapi.CommitteeCertificate{}
+	err := rpcp.client.Call(
+		&results,
+		"sonic_getCommitteeCertificates",
 		fmt.Sprintf("0x%x", first),
 		fmt.Sprintf("0x%x", maxResults),
 	)
 	if err != nil {
 		return nil, err
 	}
+	if uint64(len(results)) > maxResults {
+		return nil, fmt.Errorf("Too many certificates returned")
+	}
 	certs := []cert.CommitteeCertificate{}
+	currentPeriod := first
 	for _, res := range results {
+		if res.Period != uint64(currentPeriod) {
+			return nil, fmt.Errorf("Committee certificates out of order")
+		}
+		currentPeriod++
 		certs = append(certs, res.ToCertificate())
 	}
 	return certs, nil
@@ -94,24 +110,35 @@ func (rpcp RPCProvider) GetCommitteeCertificates(first scc.Period, maxResults ui
 // - maxResults: The maximum number of block certificates to retrieve.
 //
 // Returns:
-// - cert.BlockCertificate: The block certificates for the given block number and the following blocks.
-// - error: An error if the client is nil or the retrieval fails.
-func (rpcp RPCProvider) GetBlockCertificates(first idx.Block, maxResults uint64) ([]cert.BlockCertificate, error) {
-	if rpcp.client == nil {
+//   - cert.BlockCertificate: The block certificates for the given block number
+//     and the following blocks.
+//   - error: An error if the client is nil, the retrieval fails, the
+//     certificates are out of order or more than requested.
+func (rpcp RpcProvider) GetBlockCertificates(first idx.Block, maxResults uint64) ([]cert.BlockCertificate, error) {
+	if rpcp.IsClosed() {
 		return nil, fmt.Errorf("No client available")
 	}
 
-	results := []ethapi.BlockCertificateJson{}
-	err := rpcp.client.Client().Call(
-		&results, "sonic_getBlockCertificates",
+	results := []ethapi.BlockCertificate{}
+	err := rpcp.client.Call(
+		&results,
+		"sonic_getBlockCertificates",
 		fmt.Sprintf("0x%x", first),
 		fmt.Sprintf("0x%x", maxResults),
 	)
 	if err != nil {
 		return nil, err
 	}
+	if uint64(len(results)) > maxResults {
+		return nil, fmt.Errorf("Too many certificates returned")
+	}
 	certs := []cert.BlockCertificate{}
+	currentBlock := first
 	for _, res := range results {
+		if res.Number != uint64(currentBlock) {
+			return nil, fmt.Errorf("Block certificates out of order")
+		}
+		currentBlock++
 		certs = append(certs, res.ToCertificate())
 	}
 	return certs, nil
