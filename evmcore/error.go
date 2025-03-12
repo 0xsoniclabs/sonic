1e0cebb520e560143153149dfed94a77e16ea3ef
Luis Ayuso <luis@ph3.at>
2025-03-12 15:52:46 +0100

    Add missing check for exceeded max init code size

--- evmcore/error.go
+++ evmcore/error.go
@@ -92,4 +92,8 @@ var (
 
 	// ErrEmptyAuthorizations is returned if a SetCode transaction has no authorizations.
 	ErrEmptyAuthorizations = errors.New("empty authorizations")
+
+	// ErrMaxInitCodeSizeExceeded is returned if creation transaction provides the init code bigger
+	// than init code size limit.
+	ErrMaxInitCodeSizeExceeded = errors.New("max initcode size exceeded")
 )
