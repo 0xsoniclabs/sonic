504dff3b0ec1c192f603314d0fd8d1fb1b537d48
facuMH <facumolina15@gmail.com>
2025-03-14 15:24:34 +0100

    fix in function documentation

--- scc/light_client/light_client_state/light_client_state.go
+++ scc/light_client/light_client_state/light_client_state.go
@@ -29,14 +29,14 @@ func NewState(committee scc.Committee) *State {
 	}
 }
 
-// Head returns the block number of the latest known block.
-// The second value is true only if the state has been successfully synced.
+// Head returns the block number of the latest known block and a bool which
+// is only true if the state has already been successfully synced.
 func (s *State) Head() (idx.Block, bool) {
 	return s.headNumber, s.hasSynced
 }
 
-// StateRoot returns the state root of the latest known block.
-// The second value is true only if the state has been successfully synced.
+// StateRoot returns the state root of the latest known block and a bool which
+// is only true if the state has already been successfully synced.
 func (s *State) StateRoot() (common.Hash, bool) {
 	return s.headRoot, s.hasSynced
 }
