90c9ae5f76dd69a48d1ff207a6839a628f1eb316
Kamil Jezek <kamil.jezek@gmail.com>
2025-04-17 15:51:52 +0200

    tests rule updated on epoch

--- tests/network_rules_update_test.go
+++ tests/network_rules_update_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"encoding/json"
+	"github.com/0xsoniclabs/sonic/evmcore"
 	"github.com/0xsoniclabs/sonic/gossip/contract/driverauth100"
 	"github.com/0xsoniclabs/sonic/opera/contracts/driverauth"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -31,8 +32,9 @@ func TestNetworkRule_Update_RulesChangeDuringEpochHasNoEffect(t *testing.T) {
 	require.NoError(err)
 	require.NotEqual(0, originalRules.Economy.MinBaseFee.Int64(), "MinBaseFee should be filled")
 
+	newMinBaseFee := 1e3 * originalRules.Economy.MinBaseFee.Int64()
 	updateRequest := rulesType{}
-	updateRequest.Economy.MinBaseFee = new(big.Int).SetInt64(2 * originalRules.Economy.MinBaseFee.Int64())
+	updateRequest.Economy.MinBaseFee = new(big.Int).SetInt64(newMinBaseFee)
 
 	// Update network rules
 	updateNetworkRules(t, net, updateRequest)
@@ -44,6 +46,28 @@ func TestNetworkRule_Update_RulesChangeDuringEpochHasNoEffect(t *testing.T) {
 
 	require.Equal(originalRules.Economy.MinBaseFee, updatedRules.Economy.MinBaseFee,
 		"Network rules should not change - it must be an epoch bound")
+
+	var blockBefore evmcore.EvmBlockJson
+	err = client.Client().Call(&blockBefore, "eth_getBlockByNumber", "latest")
+	require.NoError(err)
+
+	require.Less(blockBefore.BaseFee.ToInt(), newMinBaseFee, "BaseFee should not reflect new MinBaseFee")
+
+	// apply epoch change
+	advanceEpoch(t, net)
+
+	// rule should be effective
+	err = client.Client().Call(&updatedRules, "eth_getRules", "latest")
+	require.NoError(err)
+
+	require.Equal(newMinBaseFee, updatedRules.Economy.MinBaseFee,
+		"Network rules should become effective after epoch change")
+
+	var blockAfter evmcore.EvmBlockJson
+	err = client.Client().Call(&blockAfter, "eth_getBlockByNumber", "latest")
+	require.NoError(err)
+
+	require.GreaterOrEqual(blockAfter.BaseFee.ToInt(), newMinBaseFee, "BaseFee should reflect new MinBaseFee")
 }
 
 func TestNetworkRule_Update_Restart_Recovers_Original_Value(t *testing.T) {
@@ -66,8 +90,9 @@ func TestNetworkRule_Update_Restart_Recovers_Original_Value(t *testing.T) {
 	require.NoError(err)
 	require.NotEqual(0, originalRules.Economy.MinBaseFee.Int64(), "MinBaseFee should be filled")
 
+	newMinBaseFee := 1e3 * originalRules.Economy.MinBaseFee.Int64()
 	updateRequest := rulesType{}
-	updateRequest.Economy.MinBaseFee = new(big.Int).SetInt64(2 * originalRules.Economy.MinBaseFee.Int64())
+	updateRequest.Economy.MinBaseFee = new(big.Int).SetInt64(newMinBaseFee)
 
 	// Update network rules
 	updateNetworkRules(t, net, updateRequest)
@@ -83,11 +108,27 @@ func TestNetworkRule_Update_Restart_Recovers_Original_Value(t *testing.T) {
 
 	// Network rule should not change - it must be an epoch bound
 	var updatedRules rulesType
-	err = client.Client().Call(&updatedRules, "eth_getRules", "latest")
+	err = client2.Client().Call(&updatedRules, "eth_getRules", "latest")
 	require.NoError(err)
 
 	require.Equal(originalRules.Economy.MinBaseFee, updatedRules.Economy.MinBaseFee,
 		"Network rules should not change - it must be an epoch bound")
+
+	// apply epoch change
+	advanceEpoch(t, net)
+
+	// rule should be effective
+	err = client2.Client().Call(&updatedRules, "eth_getRules", "latest")
+	require.NoError(err)
+
+	require.Equal(newMinBaseFee, updatedRules.Economy.MinBaseFee,
+		"Network rules should become effective after epoch change")
+
+	var blockAfter evmcore.EvmBlockJson
+	err = client2.Client().Call(&blockAfter, "eth_getBlockByNumber", "latest")
+	require.NoError(err)
+
+	require.GreaterOrEqual(blockAfter.BaseFee.ToInt(), newMinBaseFee, "BaseFee should reflect new MinBaseFee")
 }
 
 // updateNetworkRules sends a transaction to update the network rules.
@@ -107,6 +148,25 @@ func updateNetworkRules(t *testing.T, net IntegrationTestNetSession, rulesChange
 
 	receipt, err := net.Apply(func(ops *bind.TransactOpts) (*types.Transaction, error) {
 		return contract.UpdateNetworkRules(ops, b)
+	})
+
+	require.NoError(err)
+	require.Equal(receipt.Status, types.ReceiptStatusSuccessful)
+}
+
+func advanceEpoch(t *testing.T, net IntegrationTestNetSession) {
+	t.Helper()
+	require := require.New(t)
+
+	client, err := net.GetClient()
+	require.NoError(err)
+	defer client.Close()
+
+	contract, err := driverauth100.NewContract(driverauth.ContractAddress, client)
+	require.NoError(err)
+
+	receipt, err := net.Apply(func(ops *bind.TransactOpts) (*types.Transaction, error) {
+		return contract.AdvanceEpochs(ops, big.NewInt(1))
 	})
 
 	require.NoError(err)
