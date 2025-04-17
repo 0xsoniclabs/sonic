d1f8ea3f46002a6cacdc46cec686db437fbe6373
Herbert Jordan <herbert@swrnd.ch>
2025-04-17 17:57:56 +0200

    Improve tests to cover a cleaner specification

--- inter/turns.go
+++ inter/turns.go
@@ -31,23 +31,24 @@ func IsValidTurnProgression(
 	last ProposalSummary,
 	next ProposalSummary,
 ) bool {
-	// Turns must strictly increase in each progression step.
-	if last.Turn >= next.Turn {
+	// Turns and frames must strictly increase in each progression step.
+	if last.Turn >= next.Turn || last.Frame >= next.Frame {
 		return false
 	}
 
-	// In the good case, the subsequent proposal is for the succeeding turn
-	// in a subsequent frame. This does not require a minimum waiting period.
-	if last.Turn+1 == next.Turn {
-		return last.Frame < next.Frame
-	}
-
-	// If there is a failed turn (either not proposed or not accepted), the
-	// next turn must be at least ProposalTimeoutInterval frames after the
-	// previous turn. This is to give a proposer enough time to propose
-	// a its block before being declared failed.
-	gap := TurnTimeoutInFrames * uint64(next.Turn-last.Turn-1)
-	return uint64(last.Frame)+gap <= uint64(next.Frame)
+	// Every attempt has a window of frames (d*C,(d+1)*C] where d is the number
+	// of turns between the last and the next turn. Thus, the immediate
+	// successor of a turn can be processed in the frames (0,C] = [1,C] after
+	// the last turn. If the successor turn is not materializing, the next turn
+	// can be processed in the frames (C,2*C] = [C+1,2*C] after the last turn.
+	//
+	// This rules partition future frames into intervals of size C, each
+	// associated to a specific turn. Thus, for no future frame the last seen
+	// turn allows more than one proposal to be made. This is important to make
+	// sure that validators are not saving up proposals by not using their turns
+	// and then proposing all of them in a burst.
+	delta := uint32(next.Frame - last.Frame - 1)
+	return delta/TurnTimeoutInFrames == uint32(next.Turn-last.Turn-1)
 }
 
 // ProposalSummary is a summary of the metadata of a proposal made in a turn.
