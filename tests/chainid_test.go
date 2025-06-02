8887dca14a01e7b538bf77b89cadecbed707373b
facuMH <facumolina15@gmail.com>
2025-06-02 13:54:30 +0200

    Remove superfluous test

--- tests/chainid_test.go
+++ tests/chainid_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/0xsoniclabs/sonic/config"
 	"github.com/0xsoniclabs/sonic/ethapi"
-	"github.com/0xsoniclabs/sonic/opera"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/holiman/uint256"
@@ -139,91 +138,13 @@ func TestChainId_AcceptsLegacyTxSignedWithHomestead(t *testing.T) {
 	require.Equal(t, signed.Hash(), json.Hash)
 	// Since HomesteadSigner does not have a chain ID, the transaction should be
 	// processed and stored with chain ID 0.
-	require.Equal(t, json.ChainID.ToInt().Cmp(big.NewInt(0)), 0)
+	require.Equal(t, int64(0), json.ChainID.ToInt().Int64())
 
 	// reconstruct the transaction from the RPC response
 	// and verify that it has the same hash and chain ID as the signed transaction
 	decodedTx := rpcTransactionToTransaction(t, json)
 	require.Equal(t, signed.Hash(), decodedTx.Hash())
-	require.Equal(t, signed.ChainId().Int64(), decodedTx.ChainId().Int64())
-}
-
-func TestChainId_NonLegacyTxWithChainIdZeroAreSignedAndProcessed(t *testing.T) {
-	zeroBig := new(big.Int).Sub(big.NewInt(1), big.NewInt(1))
-	zeroUint := uint256.NewInt(0)
-
-	enoughGas := uint64(1e6)
-
-	tests := map[string]types.TxData{
-		"AccessList": &types.AccessListTx{
-			ChainID:  zeroBig,
-			GasPrice: big.NewInt(enoughGasPrice),
-			Gas:      enoughGas,
-		},
-		"DynamicFee": &types.DynamicFeeTx{
-			ChainID:   zeroBig,
-			GasFeeCap: big.NewInt(enoughGasPrice),
-			Gas:       enoughGas,
-		},
-		"Blob": &types.BlobTx{
-			ChainID:   zeroUint,
-			GasFeeCap: uint256.NewInt(enoughGasPrice),
-			Gas:       enoughGas,
-		},
-		"SetCode": &types.SetCodeTx{
-			ChainID:   zeroUint,
-			AuthList:  []types.SetCodeAuthorization{{}},
-			GasFeeCap: uint256.NewInt(enoughGasPrice),
-			Gas:       enoughGas,
-		},
-	}
-
-	net := StartIntegrationTestNet(t,
-		IntegrationTestNetOptions{Upgrades: AsPointer(opera.GetAllegroUpgrades())},
-	)
-	client, err := net.GetClient()
-	require.NoError(t, err)
-	defer client.Close()
-
-	actualChainID, err := client.ChainID(context.Background())
-	require.NoError(t, err, "failed to get chain ID")
-
-	for name, tx := range tests {
-		t.Run(name, func(t *testing.T) {
-
-			account := makeAccountWithBalance(t, net, big.NewInt(1e18))
-			// signing transaction with any types.modernSinger will change the
-			// chain ID of the signed transaction to the chain ID that was used
-			// to initialize the signer.
-			signed, err := types.SignTx(
-				types.NewTx(tx),
-				types.NewPragueSigner(actualChainID),
-				account.PrivateKey)
-			require.NoError(t, err)
-			require.Equal(t, signed.ChainId().Int64(), actualChainID.Int64())
-
-			receipt, err := net.Run(signed)
-			require.NoError(t, err, "failed to run transaction")
-			require.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
-
-			// verify that processed transaction can be retrieved by hash
-			// and that it has the correct chain ID
-			var json *ethapi.RPCTransaction
-			err = client.Client().CallContext(context.Background(), &json,
-				"eth_getTransactionByHash", signed.Hash(),
-			)
-			require.NoError(t, err)
-			require.Equal(t, signed.Hash(), json.Hash)
-			require.Equal(t, signed.ChainId().Int64(), json.ChainID.ToInt().Int64())
-			require.Equal(t, json.ChainID.ToInt().Int64(), actualChainID.Int64())
-			// reconstruct the transaction from the RPC response
-			// and verify that it has the same hash and chain ID as the signed transaction
-			decodedTx := rpcTransactionToTransaction(t, json)
-			require.Equal(t, signed.Hash(), decodedTx.Hash())
-			require.Equal(t, signed.ChainId().Int64(), decodedTx.ChainId().Int64())
-
-		})
-	}
+	require.Equal(t, int64(0), decodedTx.ChainId().Int64())
 }
 
 func rpcTransactionToTransaction(t *testing.T, tx *ethapi.RPCTransaction) *types.Transaction {
