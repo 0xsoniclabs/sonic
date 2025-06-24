f09a47bd6e17ec22692134e9435b8e2ae93c892c
facuMH <facumolina15@gmail.com>
2025-06-24 16:44:17 +0200

    Improve file handling in test

--- scripts/license/add_license_header_test.go
+++ scripts/license/add_license_header_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,9 +11,7 @@ import (
 func Test_Recognizes_GethLicense(t *testing.T) {
 	// make a temporary file
 	tmpDir := t.TempDir()
-	originalFile, err := os.Create(tmpDir + "/test_geth_license")
-	require.NoError(t, err)
-	defer func() { _ = originalFile.Close() }()
+	originalFileName := filepath.Join(tmpDir, "test_geth_license.go")
 
 	gethHeader := `Copyright 2014 The go-ethereum Authors
 				   This file is part of the go-ethereum library.
@@ -30,27 +29,21 @@ func Test_Recognizes_GethLicense(t *testing.T) {
 				    You should have received a copy of the GNU Lesser General Public License
 				    along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.`
 
-	// write a sample license header to the file
-	_, err = originalFile.WriteString(addPrefix(gethHeader, "//") + "package main")
-	require.NoError(t, err)
-	originalContent, err := os.ReadFile(originalFile.Name())
+	require.NoError(t, os.WriteFile(originalFileName, []byte(gethHeader), 0644))
+
+	originalContent, err := os.ReadFile(originalFileName)
 	require.NoError(t, err)
 
 	// create a copy of the file with the same content
-	copy, err := os.Create(tmpDir + "/test_geth_license_copy.go")
-	require.NoError(t, err)
-	defer func() { _ = copy.Close() }()
+	copyName := filepath.Join(tmpDir, "test_geth_license_copy.go")
+	require.NoError(t, copyFile(originalFileName, copyName))
 
-	err = copyFile(originalFile.Name(), copy.Name())
-	require.NoError(t, err)
+	require.NoError(t, processFiles(tmpDir, ".go", "//", gethHeader, false, false))
 
-	err = processFiles(tmpDir, ".go", "//", gethHeader, false, false)
+	contentAfter, err := os.ReadFile(copyName)
 	require.NoError(t, err)
 
-	contentAfter, err := os.ReadFile(copy.Name())
-	require.NoError(t, err)
-
-	require.Equal(t, string(originalContent), string(contentAfter))
+	require.Equal(t, originalContent, contentAfter)
 }
 
 func Test_Recognizes_CurrentSonicLicense(t *testing.T) {
@@ -58,51 +51,39 @@ func Test_Recognizes_CurrentSonicLicense(t *testing.T) {
 	tmpDir := t.TempDir()
 
 	// make a temporary file in that folder
-	tmpFile, err := os.Create(tmpDir + "/test_license")
-	require.NoError(t, err)
-	defer func() { _ = tmpFile.Close() }()
+	tmpFileName := filepath.Join(tmpDir, "test_license")
+	require.NoError(t, os.WriteFile(tmpFileName, []byte(addPrefix(licenseHeader, "//")+"\npackage main"), 0660))
 
-	_, err = tmpFile.WriteString(addPrefix(licenseHeader, "//") + "\npackage main")
-	require.NoError(t, err)
-	originalContent, err := os.ReadFile(tmpFile.Name())
+	originalContent, err := os.ReadFile(tmpFileName)
 	require.NoError(t, err)
 
 	// create a copy of the file with the same content
-	copy, err := os.Create(tmpDir + "/test_geth_license_copy.go")
-	require.NoError(t, err)
-	defer func() { _ = copy.Close() }()
+	copyName := filepath.Join(tmpDir, "test_geth_license_copy.go")
+	require.NoError(t, copyFile(tmpFileName, copyName))
 
-	err = copyFile(tmpFile.Name(), copy.Name())
-	require.NoError(t, err)
+	require.NoError(t, processFiles(tmpDir, ".go", "//", licenseHeader, false, false))
 
-	err = processFiles(tmpDir, ".go", "//", licenseHeader, false, false)
+	contentAfter, err := os.ReadFile(copyName)
 	require.NoError(t, err)
-
-	contentAfter, err := os.ReadFile(copy.Name())
-	require.NoError(t, err)
-	require.Equal(t, string(originalContent), string(contentAfter))
+	require.Equal(t, originalContent, contentAfter)
 }
 
 func Test_Replaces_OldLicenseHeader(t *testing.T) {
 	// make a temporary folder
 	tmpDir := t.TempDir()
 	// make a temporary file in that folder
-	tmpFile, err := os.Create(tmpDir + "/test_license.go")
-	require.NoError(t, err)
-	defer func() { _ = tmpFile.Close() }()
-
+	tmpFileName := filepath.Join(tmpDir, "test_license.go")
 	// write a sample license header to the file
 	oldLicense := `Copyright 2024 Sonic Operations Ltd
 				   This file is part of some old version
 				   of the Sonic Client`
-	_, err = tmpFile.WriteString(addPrefix(oldLicense, "//") + "\n\npackage main\n\nfunc main() {}\n")
-	require.NoError(t, err)
-	require.NoError(t, tmpFile.Close())
+	originalContent := []byte(addPrefix(oldLicense, "//") + "\npackage main\n")
 
-	err = processFiles(tmpDir, ".go", "//", licenseHeader, false, false)
-	require.NoError(t, err)
+	require.NoError(t, os.WriteFile(tmpFileName, originalContent, 0660))
 
-	content, err := os.ReadFile(tmpFile.Name())
+	require.NoError(t, processFiles(tmpDir, ".go", "//", licenseHeader, false, false))
+
+	content, err := os.ReadFile(tmpFileName)
 	require.NoError(t, err)
 	require.Contains(t, string(content), addPrefix(licenseHeader, "//"))
 
@@ -113,19 +94,13 @@ func Test_Adds_LicenseHeader(t *testing.T) {
 	// make a temporary folder
 	tmpDir := t.TempDir()
 	// make a temporary file in that folder
-	tmpFile, err := os.Create(tmpDir + "/test_license.go")
-	require.NoError(t, err)
-	defer func() { _ = tmpFile.Close() }()
+	tmpFileName := filepath.Join(tmpDir, "test_license.go")
+	// write a file without license header
+	require.NoError(t, os.WriteFile(tmpFileName, []byte("package main\n\nfunc main() {}\n"), 0660))
 
-	// write a sample license header to the file
-	_, err = tmpFile.WriteString("package main\n\nfunc main() {}\n")
-	require.NoError(t, err)
-	require.NoError(t, tmpFile.Close())
+	require.NoError(t, processFiles(tmpDir, ".go", "//", licenseHeader, false, false))
 
-	err = processFiles(tmpDir, ".go", "//", licenseHeader, false, false)
-	require.NoError(t, err)
-
-	content, err := os.ReadFile(tmpFile.Name())
+	content, err := os.ReadFile(tmpFileName)
 	require.NoError(t, err)
 	extendLicenseHeader := addPrefix(licenseHeader, "//")
 	require.Contains(t, string(content), extendLicenseHeader)
@@ -136,39 +111,35 @@ func copyFile(src, dst string) error {
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(dst, originalContent, 0644)
+	return os.WriteFile(dst, originalContent, 0440)
 }
 
 func Test_Detects_DoubleHeader(t *testing.T) {
 	// make a temporary folder
 	tmpDir := t.TempDir()
 	// make a temporary file in that folder
-	tmpFile, err := os.Create(tmpDir + "/test_license.go")
-	require.NoError(t, err)
-	defer func() { _ = tmpFile.Close() }()
-
-	// write a sample license header to the file
-	_, err = tmpFile.WriteString(addPrefix(licenseHeader, "//") + addPrefix(licenseHeader, "//") + "\npackage main")
-	require.NoError(t, err)
+	tmpFileName := filepath.Join(tmpDir, "test_license.go")
+	doubleHeaderString := addPrefix(licenseHeader, "//") +
+		addPrefix(licenseHeader, "//") +
+		"\npackage main"
+	require.NoError(t, os.WriteFile(tmpFileName, []byte(doubleHeaderString), 0660))
 
 	// Check for double license headers
-	require.Error(t, checkDoubleHeader(tmpFile.Name(), "//"))
+	require.Error(t, checkDoubleHeader(tmpFileName, "//"))
 }
 
-func Test_OnlyOnEmptyLineAfterHeader(t *testing.T) {
+func Test_OnlyOneEmptyLineAfterHeader(t *testing.T) {
 	// make a temporary folder
 	tmpDir := t.TempDir()
 	// make a temporary file in that folder
-	tmpFile, err := os.Create(tmpDir + "/test_license.go")
-	require.NoError(t, err)
-	defer func() { _ = tmpFile.Close() }()
+	tmpFileName := filepath.Join(tmpDir, "test_license.go")
 
-	// write a sample license header to the file
-	_, err = tmpFile.WriteString(addPrefix(licenseHeader, "//") + "\npackage main\n\nfunc main() {}\n")
-	require.NoError(t, err)
+	fileWithHeader := addPrefix(licenseHeader, "//") + "\npackage main\nfunc main() {}\n"
+	require.NoError(t, os.WriteFile(tmpFileName, []byte(fileWithHeader), 0660))
 
-	// Check for double license headers
-	err = processFiles(tmpDir, ".go", "//", licenseHeader, false, false)
+	require.NoError(t, processFiles(tmpDir, ".go", "//", licenseHeader, false, false))
+
+	content, err := os.ReadFile(tmpFileName)
 	require.NoError(t, err)
 
 	content, err := os.ReadFile(tmpFile.Name())
