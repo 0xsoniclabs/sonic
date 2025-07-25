db25b9e75fe3f0293e0b4fd3c5dadc4a7f843c49
Luis Ayuso <luis@ph3.at>
2025-07-25 09:54:10 +0200

    Remove unused solidity log

--- tests/contracts/data_reader/data_reader.sol
+++ tests/contracts/data_reader/data_reader.sol
@@ -4,12 +4,5 @@ pragma solidity ^0.8.20;
 // DataReader is a contract that reads data from the transaction data field.
 // It can be used to test transactions with different data input sizes.
 contract DataReader {
-    event DataSize(uint64 size, uint64 bufferSize);
-
-    function sendData(bytes memory data) public {
-        // Emit both the size of the raw transaction data field and
-        // the size of the data passed to this function.
-        // This helps tuning the caller side to approach limits.
-        emit DataSize(uint64(msg.data.length), uint64(data.length));
-    }
+    function sendData(bytes memory data) public {}
 }
