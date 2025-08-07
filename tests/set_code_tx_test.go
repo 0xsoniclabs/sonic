e78a48b1a16d12d72a97aa46450e9dd47db3b577
Facu <facumolina15@gmail.com>
2025-08-07 11:51:54 +0200

    Revert "Use sessions in set code integration test (#429)"
    This reverts commit f544f0bb0cd73925deb7e89ab98be70f0ee19a0b.

--- tests/set_code_tx_test.go
+++ tests/set_code_tx_test.go
@@ -50,57 +50,58 @@ import (
 // and do not implement ERC-20 as described in the EIP use case examples.
 func TestSetCodeTransaction(t *testing.T) {
 
-	session := getIntegrationTestNetSession(t, opera.GetAllegroUpgrades())
+	net := StartIntegrationTestNet(t, IntegrationTestNetOptions{
+		Upgrades: AsPointer(opera.GetAllegroUpgrades()),
+	})
 
 	t.Run("Operation", func(t *testing.T) {
 		t.Parallel()
 		// operation tests check basic operation of the SetCode transaction
 
 		t.Run("Delegate can be set and unset", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testDelegateCanBeSetAndUnset(t, session)
 		})
 
 		t.Run("Invalid authorizations are ignored", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
-			testInvalidAuthorizationsAreIgnored(t, session)
+			testInvalidAuthorizationsAreIgnored(t, net)
 		})
 
 		t.Run("Authorizations are executed in order", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testAuthorizationsAreExecutedInOrder(t, session)
 		})
 
 		t.Run("Multiple accounts can submit authorizations", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testMultipleAccountsCanSubmitAuthorizations(t, session)
 		})
 
 		t.Run("Authorization succeeds with failing tx", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testAuthorizationSucceedsWithFailingTx(t, session)
 		})
 
 		t.Run("Authorization can be issued from a non existing account", func(t *testing.T) {
-			session := session.SpawnSession(t)
-			testAuthorizationFromNonExistingAccount(t, session)
 			t.Parallel()
+			session := net.SpawnSession(t)
+			testAuthorizationFromNonExistingAccount(t, session)
 		})
 
 		t.Run("Delegations cannot be transitive", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testNoDelegateToDelegated(t, session)
 		})
 
 		t.Run("Delegations can trigger chains of calls", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testChainOfCalls(t, session)
 		})
 
@@ -111,20 +112,20 @@ func TestSetCodeTransaction(t *testing.T) {
 		// UseCase tests check the use cases described in the EIP-7702 specification
 
 		t.Run("Transaction Sponsoring", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testSponsoring(t, session)
 		})
 
 		t.Run("Transaction Batching", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testBatching(t, session)
 		})
 
 		t.Run("Privilege Deescalation", func(t *testing.T) {
-			session := session.SpawnSession(t)
 			t.Parallel()
+			session := net.SpawnSession(t)
 			testPrivilegeDeescalation(t, session)
 		})
 	})
@@ -394,9 +395,9 @@ func testDelegateCanBeSetAndUnset(t *testing.T, session IntegrationTestNetSessio
 
 // testInvalidAuthorizationsAreIgnored checks that invalid authorizations are ignored
 // whilst the transaction is still executed.
-func testInvalidAuthorizationsAreIgnored(t *testing.T, session IntegrationTestNetSession) {
+func testInvalidAuthorizationsAreIgnored(t *testing.T, net *IntegrationTestNet) {
 
-	client, err := session.GetClient()
+	client, err := net.GetClient()
 	require.NoError(t, err)
 	defer client.Close()
 
@@ -520,8 +521,8 @@ func testInvalidAuthorizationsAreIgnored(t *testing.T, session IntegrationTestNe
 	for caseName, test := range wrongAuthorizations {
 		for scenarioName, scenario := range scenarios {
 			t.Run(fmt.Sprintf("%s/%s", caseName, scenarioName), func(t *testing.T) {
-				session := session.SpawnSession(t)
 				t.Parallel()
+				session := net.SpawnSession(t)
 
 				wrongAuthAccount := makeAccountWithBalance(t, session, big.NewInt(1e18)) // < will transfer funds
 				nonce, err := client.NonceAt(t.Context(), wrongAuthAccount.Address(), nil)
