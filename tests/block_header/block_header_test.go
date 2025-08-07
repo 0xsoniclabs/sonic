5eb0877a555126d114ab8cb3d529898130eb69e5
facuMH <facumolina15@gmail.com>
2025-08-07 11:32:12 +0200

    Move block header integration tests to their own package

--- tests/block_header/block_header_test.go
+++ tests/block_header/block_header_test.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Sonic. If not, see <http://www.gnu.org/licenses/>.
 
-package tests
+package block_header_test
 
 import (
 	"cmp"
@@ -22,19 +22,17 @@ import (
 	"math/big"
 	"slices"
 	"testing"
-	"time"
 
 	"github.com/0xsoniclabs/carmen/go/carmen"
 	"github.com/0xsoniclabs/carmen/go/common/immutable"
 	"github.com/0xsoniclabs/carmen/go/database/mpt"
-	"github.com/0xsoniclabs/sonic/gossip/gasprice"
-	"github.com/0xsoniclabs/sonic/inter"
 	"github.com/0xsoniclabs/sonic/opera"
 	"github.com/0xsoniclabs/sonic/opera/contracts/driver"
 	"github.com/0xsoniclabs/sonic/opera/contracts/driverauth"
 	"github.com/0xsoniclabs/sonic/opera/contracts/evmwriter"
 	"github.com/0xsoniclabs/sonic/opera/contracts/netinit"
 	"github.com/0xsoniclabs/sonic/opera/contracts/sfc"
+	. "github.com/0xsoniclabs/sonic/tests"
 	"github.com/0xsoniclabs/sonic/tests/contracts/counter_event_emitter"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -48,11 +46,15 @@ import (
 )
 
 func TestBlockHeader_FakeGenesis_SatisfiesInvariants(t *testing.T) {
+	t.Parallel()
+
 	net := StartIntegrationTestNetWithFakeGenesis(t)
 	testBlockHeadersOnNetwork(t, net)
 }
 
 func TestBlockHeader_JsonGenesis_SatisfiesInvariants(t *testing.T) {
+	t.Parallel()
+
 	upgrades := map[string]opera.Upgrades{
 		"Sonic":   opera.GetSonicUpgrades(),
 		"Allegro": opera.GetAllegroUpgrades(),
@@ -228,88 +230,6 @@ func testHeaders_CompareHeaderHashes(t *testing.T, hashes []common.Hash, newHead
 	}
 }
 
-func testHeaders_BlockNumberEqualsPositionInChain(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-	for i, header := range headers {
-		require.Equal(header.Number.Uint64(), uint64(i))
-	}
-}
-
-func testHeaders_ParentHashCoversParentContent(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-
-	// The parent hash of block 0 is expected to be zero.
-	require.Equal(
-		headers[0].ParentHash, common.Hash{},
-		"invalid parent hash for block 0",
-	)
-
-	// All other blocks have a parent hash that matches the previous block's hash.
-	for i := 1; i < len(headers); i++ {
-		require.Equal(
-			headers[i].ParentHash,
-			headers[i-1].Hash(),
-			"invalid hash stored in block %d for block %d", i, i-1,
-		)
-	}
-}
-
-func testHeaders_GasUsedIsBelowGasLimit(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-	for i, header := range headers {
-		require.LessOrEqual(header.GasUsed, header.GasLimit, "block %d", i)
-	}
-}
-
-func testHeaders_EncodesDurationAndNanoTimeInExtraData(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-
-	getUnixTime := func(header *types.Header) time.Time {
-		t.Helper()
-		nanos, _, err := inter.DecodeExtraData(header.Extra)
-		require.NoError(err)
-		return time.Unix(int64(header.Time), int64(nanos))
-	}
-
-	// Check the nano-time and duration encoded in the extra data field.
-	for i := 1; i < len(headers); i++ {
-		require.Equal(len(headers[i].Extra), 12, "extra data length of block %d", i)
-		lastTime := getUnixTime(headers[i-1])
-		currentTime := getUnixTime(headers[i])
-		wantedDuration := currentTime.Sub(lastTime)
-		_, gotDuration, err := inter.DecodeExtraData(headers[i].Extra)
-		require.NoError(err, "decoding extra data of block %d", i)
-		require.Equal(wantedDuration, gotDuration, "duration of block %d", i)
-	}
-}
-
-func testHeaders_BaseFeeEvolutionFollowsPricingRules(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-
-	// The genesis block must use the initial base fee.
-	rules := opera.FakeEconomyRules()
-	require.Equal(
-		gasprice.GetInitialBaseFee(rules),
-		headers[0].BaseFee,
-	)
-
-	// All other blocks compute the base-fee based on the previous block.
-	for i := 1; i < len(headers); i++ {
-		_, duration, err := inter.DecodeExtraData(headers[i-1].Extra)
-		require.NoError(err, "decoding extra data of block %d", i-1)
-		last := gasprice.ParentBlockInfo{
-			BaseFee:  headers[i-1].BaseFee,
-			GasUsed:  headers[i-1].GasUsed,
-			Duration: duration,
-		}
-		require.Equal(
-			gasprice.GetBaseFeeForNextBlock(last, rules),
-			headers[i].BaseFee,
-			"base fee of block %d", i,
-		)
-	}
-}
-
 func testHeaders_TransactionRootMatchesBlockTxsHash(t *testing.T, headers []*types.Header, client *PooledEhtClient) {
 	require := require.New(t)
 
@@ -405,22 +325,6 @@ func testHeaders_NonceIsZeroForAllBlocks(t *testing.T, headers []*types.Header) 
 
 	for _, header := range headers {
 		require.Zero(header.Nonce.Uint64(), "nonce is not zero")
-	}
-}
-
-func testHeaders_TimeProgressesMonotonically(t *testing.T, headers []*types.Header) {
-	require := require.New(t)
-
-	getTimeFrom := func(header *types.Header) time.Time {
-		currentNano, _, err := inter.DecodeExtraData(header.Extra)
-		require.NoError(err)
-		return time.Unix(int64(header.Time), int64(currentNano))
-	}
-
-	for i := 1; i < len(headers); i++ {
-		currentTime := getTimeFrom(headers[i])
-		previousTime := getTimeFrom(headers[i-1])
-		require.Greater(currentTime, previousTime, "time is not monotonically increasing. block %d", i)
 	}
 }
 
