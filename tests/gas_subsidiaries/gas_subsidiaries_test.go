89b875e317f3d92b686c463bb24dfca8fee12855
facuMH <facumolina15@gmail.com>
2025-09-19 10:21:57 +0200

    Introduce feature flag for gas subsidies

---
+++ tests/gas_subsidiaries/gas_subsidiaries_test.go
@@ -0,0 +1,87 @@
+package gassubsidiaries
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/0xsoniclabs/sonic/opera"
+	"github.com/0xsoniclabs/sonic/tests"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGasSubsidies_CanBeEnabledAndDisabled(t *testing.T) {
+	upgrades := map[string]opera.Upgrades{
+		"Sonic":   opera.GetSonicUpgrades(),
+		"Allegro": opera.GetAllegroUpgrades(),
+		// "Brio":    opera.GetBrioUpgrades(),
+	}
+
+	for name, upgrades := range upgrades {
+		t.Run(name, func(t *testing.T) {
+
+			for _, numNodes := range []int{1, 3} {
+				t.Run(fmt.Sprintf("numNodes=%d", numNodes), func(t *testing.T) {
+					testGasSubsidies_CanBeEnabledAndDisabled(t, numNodes, upgrades)
+				})
+			}
+		})
+	}
+}
+
+func testGasSubsidies_CanBeEnabledAndDisabled(
+	t *testing.T,
+	numNodes int,
+	mode opera.Upgrades,
+) {
+	require := require.New(t)
+
+	// The network is initially started using the distributed protocol.
+	mode.SingleProposerBlockFormation = false
+	net := tests.StartIntegrationTestNet(t, tests.IntegrationTestNetOptions{
+		NumNodes: numNodes,
+		Upgrades: &mode,
+	})
+
+	client, err := net.GetClient()
+	require.NoError(err)
+	defer client.Close()
+
+	// check original state
+	type upgrades struct {
+		GasSubsidies bool
+	}
+	type rulesType struct {
+		Upgrades upgrades
+	}
+
+	var originalRules rulesType
+	err = client.Client().Call(&originalRules, "eth_getRules", "latest")
+	require.NoError(err)
+	require.Equal(false, originalRules.Upgrades.GasSubsidies, "GasSubsidies should be disabled initially")
+
+	// Enable gas subsidies.
+	rulesDiff := rulesType{
+		Upgrades: upgrades{GasSubsidies: true},
+	}
+	tests.UpdateNetworkRules(t, net, rulesDiff)
+
+	// Advance the epoch by one to apply the change.
+	net.AdvanceEpoch(t, 1)
+
+	err = client.Client().Call(&originalRules, "eth_getRules", "latest")
+	require.NoError(err)
+	require.Equal(true, originalRules.Upgrades.GasSubsidies, "GasSubsidies should be disabled initially")
+
+	// Disable gas subsidies.
+	rulesDiff = rulesType{
+		Upgrades: upgrades{GasSubsidies: false},
+	}
+	tests.UpdateNetworkRules(t, net, rulesDiff)
+
+	// Advance the epoch by one to apply the change.
+	net.AdvanceEpoch(t, 1)
+
+	err = client.Client().Call(&originalRules, "eth_getRules", "latest")
+	require.NoError(err)
+	require.Equal(false, originalRules.Upgrades.GasSubsidies, "GasSubsidies should be disabled initially")
+}
