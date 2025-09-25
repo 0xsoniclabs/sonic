45fbe8f518ea62e058a57e407ce28f56b25c8ccb
facuMH <facumolina15@gmail.com>
2025-09-25 16:12:04 +0200

    Add gas subsidies tx index in receipt test.

--- tests/gas_subsidies/gas_subsidies_test.go
+++ tests/gas_subsidies/gas_subsidies_test.go
@@ -18,12 +18,16 @@ package gassubsidies
 
 import (
 	"math/big"
+	"slices"
 	"testing"
 
-	"github.com/0xsoniclabs/carmen/go/common"
 	"github.com/0xsoniclabs/sonic/config"
+	"github.com/0xsoniclabs/sonic/gossip/blockproc/subsidies/registry"
 	"github.com/0xsoniclabs/sonic/opera"
 	"github.com/0xsoniclabs/sonic/tests"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -161,7 +165,7 @@ func TestGasSubsidies_SubsidizedTransactionDeductsSubsidyFunds(t *testing.T) {
 	)
 }
 
-func TestGasSubsidies_InternalTransactionHasConsecutiveNonce(t *testing.T) {
+func TestGasSubsidies_InternalTransaction_ConsecutiveNonces(t *testing.T) {
 	require := require.New(t)
 
 	upgrades := opera.GetAllegroUpgrades()
@@ -182,7 +186,7 @@ func TestGasSubsidies_InternalTransactionHasConsecutiveNonce(t *testing.T) {
 	// set up sponsorship
 	createRegistryWithDonation(t, client, net, sponsor, sponsee, receiver, donation)
 
-	interanlNonce, err := client.PendingNonceAt(t.Context(), common.Address{})
+	internalNonce, err := client.PendingNonceAt(t.Context(), common.Address{})
 	require.NoError(err)
 
 	receipt := sendSponsoredTransactionWithNonce(t, net, receiver.Address(), sponsee, 0)
@@ -194,7 +198,7 @@ func TestGasSubsidies_InternalTransactionHasConsecutiveNonce(t *testing.T) {
 	// check that the payment transaction has the same nonce as the internal transaction
 	payment := block.Transactions()[txIndex+1]
 	require.False(payment.Protected()) // should be a nonce-signed transaction
-	require.Equal(interanlNonce, payment.Nonce(),
+	require.Equal(internalNonce, payment.Nonce(),
 		"the payment transaction should have the same nonce as the internal transaction",
 	)
 
@@ -207,7 +211,193 @@ func TestGasSubsidies_InternalTransactionHasConsecutiveNonce(t *testing.T) {
 	)
 	payment = block.Transactions()[txIndex+1]
 	require.False(payment.Protected()) // should be a nonce-signed transaction
-	require.Equal(interanlNonce+1, payment.Nonce(),
+	require.Equal(internalNonce+1, payment.Nonce(),
 		"the payment transaction should have nonce incremented by 1",
 	)
+}
+
+func TestGasSubsidies_InternalTransaction_ConsistentReceipts(t *testing.T) {
+	require := require.New(t)
+
+	upgrades := opera.GetAllegroUpgrades()
+	upgrades.GasSubsidies = true
+	net := tests.StartIntegrationTestNet(t, tests.IntegrationTestNetOptions{
+		Upgrades: &upgrades,
+	})
+
+	client, err := net.GetClient()
+	require.NoError(err)
+	defer client.Close()
+
+	sponsor := tests.NewAccount()
+	sponsee := tests.NewAccount()
+	receiver := tests.NewAccount()
+	receiverAddress := receiver.Address()
+	donation := big.NewInt(1e16)
+
+	anotherAccount := tests.MakeAccountWithBalance(t, net, big.NewInt(1e18))
+
+	// set up sponsorship
+	createRegistryWithDonation(t, client, net, sponsor, sponsee, receiver, donation)
+	suggestedGasPrice, err := client.SuggestGasPrice(t.Context())
+	require.NoError(err)
+
+	makeNonSponsoredTransaction := func(nonce uint64) *types.Transaction {
+		signer := types.LatestSignerForChainID(net.GetChainId())
+		tx, err := types.SignNewTx(anotherAccount.PrivateKey, signer, &types.LegacyTx{
+			To:       &receiverAddress,
+			Gas:      21000,
+			GasPrice: suggestedGasPrice,
+			Nonce:    nonce,
+		})
+		require.NoError(err)
+		return tx
+	}
+	_ = makeNonSponsoredTransaction(0)
+
+	numTxs := 5
+	hashes := []common.Hash{}
+	for i := uint64(0); i < uint64(numTxs); i++ {
+		tx := makeNonSponsoredTransaction(i)
+		hashes = append(hashes, tx.Hash())
+		require.NoError(client.SendTransaction(t.Context(), tx), "failed to send transaction %v", i)
+
+		tx = makeSponsoredTransactionWithNonce(t, net, receiverAddress, sponsee, i)
+		hashes = append(hashes, tx.Hash())
+		require.NoError(client.SendTransaction(t.Context(), tx), "failed to send transaction %v", i)
+	}
+
+	// wait for all of them to be processed
+	receipts, err := net.GetReceipts(hashes)
+	require.NoError(err)
+
+	block, err := client.BlockByNumber(t.Context(), receipts[0].BlockNumber)
+	require.NoError(err)
+
+	for i, tx := range block.Transactions() {
+
+		receipt, err := client.TransactionReceipt(t.Context(), tx.Hash())
+		require.NoError(err, "failed to get receipt for tx %d", i)
+		require.Equal(uint(i), receipt.TransactionIndex,
+			"receipt index does not match transaction index for tx %d", i,
+		)
+
+		// if this is a payment transaction, the one before must be the sponsored tx
+		if !tx.Protected() {
+			require.True(block.Transactions()[i-1].Protected(),
+				"payment transaction at index %d must be preceded by a sponsored transaction", i,
+			)
+		}
+
+		// verify that transaction index in the block is the one reported by the receipt
+		// for internal payment as well as for non-sponsored transactions
+		require.EqualValues(i, receipt.TransactionIndex)
+		require.Equal(receipt.TxHash, tx.Hash(),
+			"receipt tx hash does not match transaction hash for tx %d", i,
+		)
+	}
+
+}
+
+func createRegistryWithDonation(t *testing.T, client *tests.PooledEhtClient, net *tests.IntegrationTestNet,
+	sponsor, sponsee, receiver *tests.Account, donation *big.Int) *registry.Registry {
+	registry, err := registry.NewRegistry(registry.GetAddress(), client)
+	require.NoError(t, err)
+
+	receipt, err := net.EndowAccount(sponsor.Address(), big.NewInt(1e18))
+	require.NoError(t, err)
+	require.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
+
+	receipt, err = net.Apply(func(opts *bind.TransactOpts) (*types.Transaction, error) {
+		opts.Value = big.NewInt(1e16)
+		return registry.SponsorUser(opts, sponsee.Address(), receiver.Address())
+	})
+	require.NoError(t, err)
+	require.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
+
+	// check that the sponsorship funds got deposited
+	sponsorship, err := registry.UserSponsorships(nil, sponsee.Address(), receiver.Address())
+	require.NoError(t, err)
+	require.Equal(t, donation, sponsorship.Funds)
+
+	return registry
+}
+
+func sendSponsoredTransaction(t *testing.T, client *tests.PooledEhtClient, net *tests.IntegrationTestNet, tx *types.Transaction) *types.Receipt {
+	require.NoError(t, client.SendTransaction(t.Context(), tx))
+
+	// Wait for the sponsored transaction to be executed.
+	receipt, err := net.GetReceipt(tx.Hash())
+	require.NoError(t, err)
+	require.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
+
+	block, err := client.BlockByNumber(t.Context(), receipt.BlockNumber)
+	require.NoError(t, err)
+	require.True(t, slices.ContainsFunc(
+		block.Transactions(),
+		func(cur *types.Transaction) bool {
+			return cur.Hash() == tx.Hash()
+		},
+	))
+
+	// Check that the payment transaction is included right after the sponsored
+	// transaction and that it was successful and has a non-zero value.
+	found := false
+	for i, tx := range block.Transactions() {
+		if tx.Hash() == receipt.TxHash {
+			require.Less(t, i, len(block.Transactions()))
+			payment := block.Transactions()[i+1]
+			receipt, err := net.GetReceipt(payment.Hash())
+			require.NoError(t, err)
+			require.Less(t, receipt.GasUsed, uint64(100_000))
+			require.Equal(t, types.ReceiptStatusSuccessful, receipt.Status)
+			found = true
+			break
+		}
+	}
+	require.True(t, found, "sponsored transaction not found in the block")
+
+	return receipt
+}
+
+func sendSponsoredTransactionWithNonce(t *testing.T, net *tests.IntegrationTestNet, receiver common.Address, sender *tests.Account, nonce uint64) *types.Receipt {
+	require := require.New(t)
+
+	sponsoredTx := makeSponsoredTransactionWithNonce(t, net, receiver, sender, nonce)
+
+	client, err := net.GetClient()
+	require.NoError(err)
+	defer client.Close()
+
+	receipt := sendSponsoredTransaction(t, client, net, sponsoredTx)
+	return receipt
+}
+
+func makeSponsoredTransactionWithNonce(t *testing.T, net *tests.IntegrationTestNet, receiver common.Address, sender *tests.Account, nonce uint64) *types.Transaction {
+	require := require.New(t)
+
+	signer := types.LatestSignerForChainID(net.GetChainId())
+	sponsoredTx, err := types.SignNewTx(sender.PrivateKey, signer, &types.LegacyTx{
+		To:       &receiver,
+		Gas:      21000,
+		GasPrice: big.NewInt(0),
+		Nonce:    nonce,
+	})
+	require.NoError(err)
+	return sponsoredTx
+}
+
+func getTransactionIndexInBlock(t *testing.T, client *tests.PooledEhtClient, receipt *types.Receipt) (int, *types.Block) {
+	require := require.New(t)
+
+	block, err := client.BlockByNumber(t.Context(), receipt.BlockNumber)
+	require.NoError(err)
+
+	for i, tx := range block.Transactions() {
+		if tx.Hash() == receipt.TxHash {
+			return i, block
+		}
+	}
+	require.Fail("transaction not found in block")
+	return -1, nil
 }
