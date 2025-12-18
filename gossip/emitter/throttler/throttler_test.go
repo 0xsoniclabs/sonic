419d51886f955e809b3e3004dc72a74a2dfb54ec
Luis Ayuso <luis@ph3.at>
2025-12-18 10:21:48 +0100

    Introduce attendance list

---
+++ gossip/emitter/throttler/throttler_test.go
@@ -0,0 +1,240 @@
+// Copyright 2025 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package throttler
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/0xsoniclabs/sonic/gossip/emitter/config"
+	"github.com/0xsoniclabs/sonic/inter"
+	"github.com/0xsoniclabs/sonic/opera"
+	"github.com/Fantom-foundation/lachesis-base/inter/idx"
+	"github.com/Fantom-foundation/lachesis-base/inter/pos"
+	"github.com/stretchr/testify/require"
+)
+
+func TestThrottler_updateAttendance_DominatingValidatorsAreOffline_AfterDominatingTimeout(t *testing.T) {
+	t.Parallel()
+
+	const currentAttempt = 15
+
+	type testCase struct {
+		stalledTimeout config.Attempt
+		lastAttendance validatorAttendance
+		expectedOnline bool
+	}
+	tests := make(map[string]testCase)
+	for lastSeenAt := config.Attempt(1); lastSeenAt <= currentAttempt; lastSeenAt++ {
+		for _, DominatingTimeout := range []config.Attempt{1, 2, 3, 4, 5} {
+			tests[fmt.Sprintf(
+				"lastSeenAt=%d stalledTimeout=%d",
+				lastSeenAt,
+				DominatingTimeout,
+			)] = testCase{
+				stalledTimeout: DominatingTimeout,
+				lastAttendance: validatorAttendance{
+					lastSeenSeq: 123,
+					lastSeenAt:  lastSeenAt,
+					online:      true,
+				},
+				expectedOnline: lastSeenAt+DominatingTimeout > currentAttempt,
+			}
+		}
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			world := testFakeWorld{
+				1: makeTestEvent(123),
+			}
+
+			config := config.ThrottlerConfig{
+				Enabled:                true,
+				DominantStakeThreshold: 0.75,
+				DominatingTimeout:      test.stalledTimeout,
+				NonDominatingTimeout:   100, // fix long timeout
+			}
+
+			attendanceList := make(attendanceList)
+			attendanceList[1] = test.lastAttendance
+			lastDominantSet := makeSet(1)
+
+			attendanceList.updateAttendance(world, config, lastDominantSet, currentAttempt)
+
+			attendance, found := attendanceList[1]
+			online := found && attendance.online
+			require.Equal(t, test.expectedOnline, online)
+		})
+	}
+}
+
+func TestThrottler_updateAttendance_SuppressedValidatorsAreOffline_AfterNonDominatingTimeout(t *testing.T) {
+	t.Parallel()
+
+	const currentAttempt = 101
+
+	type testCase struct {
+		NonDominatingTimeout config.Attempt
+		lastAttendance       validatorAttendance
+		expectedOnline       bool
+	}
+	tests := make(map[string]testCase)
+	for lastSeenAt := config.Attempt(1); lastSeenAt <= currentAttempt; lastSeenAt++ {
+		for _, NonDominatingTimeout := range []config.Attempt{1, 2, 3, 8, 10, 20, 50, 100} {
+
+			tests[fmt.Sprintf(
+				"lastSeenAt=%d NonDominatingTimeout=%d",
+				lastSeenAt,
+				NonDominatingTimeout,
+			)] = testCase{
+				NonDominatingTimeout: NonDominatingTimeout,
+				lastAttendance: validatorAttendance{
+					lastSeenSeq: 123,
+					lastSeenAt:  lastSeenAt,
+					online:      true,
+				},
+				expectedOnline: lastSeenAt+NonDominatingTimeout > currentAttempt,
+			}
+		}
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			world := testFakeWorld{
+				1: makeTestEvent(123),
+			}
+
+			config := config.ThrottlerConfig{
+				Enabled:                true,
+				DominantStakeThreshold: 0.75, // this test is stake agnostic
+				DominatingTimeout:      3,    // fix short timeout
+				NonDominatingTimeout:   test.NonDominatingTimeout,
+			}
+
+			attendanceList := make(attendanceList)
+			attendanceList[1] = test.lastAttendance
+
+			attendanceList.updateAttendance(world, config, nil, currentAttempt)
+
+			attendance, found := attendanceList[1]
+			online := found && attendance.online
+			require.Equal(t, test.expectedOnline, online)
+		})
+	}
+}
+
+func TestThrottler_updateAttendance_OfflineValidatorsComeBackOnlineWithAnyNewSeqNumber(t *testing.T) {
+	t.Parallel()
+
+	const currentAttempt = 100
+
+	type testCase struct {
+		NonDominatingTimeout config.Attempt
+		lastAttendance       validatorAttendance
+	}
+	tests := make(map[string]testCase)
+	for lastSeenAt := config.Attempt(1); lastSeenAt <= currentAttempt; lastSeenAt++ {
+		for _, DominatingTimeout := range []config.Attempt{1, 2, 3, 8, 10} {
+			for _, NonDominatingTimeout := range []config.Attempt{1, 2, 3, 8, 10} {
+
+				tests[fmt.Sprintf(
+					"lastSeenAt=%d NonDominatingTimeout=%d DominatingTimeout=%d",
+					lastSeenAt,
+					NonDominatingTimeout,
+					DominatingTimeout,
+				)] = testCase{
+					NonDominatingTimeout: DominatingTimeout,
+					lastAttendance: validatorAttendance{
+						lastSeenSeq: 122, // one less the event about to be seen
+						lastSeenAt:  lastSeenAt,
+						online:      false,
+					},
+				}
+			}
+		}
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			world := testFakeWorld{
+				1: makeTestEvent(123),
+			}
+
+			config := config.ThrottlerConfig{
+				Enabled:                true,
+				DominantStakeThreshold: 0.75,
+				DominatingTimeout:      3,
+				NonDominatingTimeout:   test.NonDominatingTimeout,
+			}
+
+			attendanceList := make(attendanceList)
+			attendanceList[1] = test.lastAttendance
+
+			attendanceList.updateAttendance(world, config, nil, currentAttempt)
+
+			attendance, found := attendanceList[1]
+			online := found && attendance.online
+			require.True(t, online)
+		})
+	}
+
+}
+
+func makeTestEvent(seq idx.Event) inter.Event {
+	builder := &inter.MutableEventPayload{}
+	builder.SetSeq(seq)
+	return builder.Build().Event
+}
+
+// testFakeWorld is a simple implementation of WorldReader for testing purposes.
+// Is is a collection of last events per validator.
+type testFakeWorld map[idx.ValidatorID]inter.Event
+
+func (fw testFakeWorld) GetEpochValidators() (*pos.Validators, idx.Epoch) {
+	builder := pos.NewBuilder()
+	for id := range fw {
+		builder.Set(id, 100)
+	}
+	return builder.Build(), idx.Epoch(0)
+}
+
+func (fw testFakeWorld) GetLastEvent(validatorID idx.ValidatorID) *inter.Event {
+	event, found := fw[validatorID]
+	if !found {
+		return nil
+	}
+	return &event
+}
+
+func (fw testFakeWorld) GetRules() opera.Rules {
+	return opera.Rules{}
+}
+
+func makeSet(ids ...idx.ValidatorID) dominantSet {
+	res := make(dominantSet)
+	for _, id := range ids {
+		res[id] = struct{}{}
+	}
+	return res
+}
