dbfab91c2c6c31ea69c72f1bd3a82f6bf6a8348b
Luis Ayuso <luis@ph3.at>
2025-12-19 10:12:43 +0100

    Add event throttling integration test

--- tests/test_utils.go
+++ tests/test_utils.go
@@ -30,12 +30,14 @@ import (
 	"github.com/0xsoniclabs/sonic/gossip/contract/driverauth100"
 	"github.com/0xsoniclabs/sonic/opera"
 	"github.com/0xsoniclabs/sonic/opera/contracts/driverauth"
+	"github.com/Fantom-foundation/lachesis-base/hash"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/holiman/uint256"
 	"github.com/stretchr/testify/require"
 )
@@ -490,4 +492,31 @@ func WaitForProofOf(t *testing.T, client *PooledEhtClient, blockNumber int) {
 		return true, nil
 	})
 	require.NoError(t, err, "failed to get witness proof")
+}
+
+func GetLatestEvents(t *testing.T, client *PooledEhtClient) []hash.Event {
+
+	// Get the current epoch.
+	block := struct {
+		Epoch hexutil.Uint64
+	}{}
+	err := client.Client().Call(&block, "eth_getBlockByNumber", rpc.BlockNumber(-1), false)
+	require.NoError(t, err)
+
+	return GetEventsFromEpoch(t, client, int(block.Epoch))
+}
+
+func GetEventsFromEpoch(t *testing.T, client *PooledEhtClient, epoch int) []hash.Event {
+
+	// Get the head res of the given epoch.
+	res := []string{}
+	err := client.Client().Call(&res, "dag_getHeads", rpc.BlockNumber(epoch))
+	require.NoError(t, err)
+
+	events := make([]hash.Event, len(res))
+	for i, eventIDStr := range res {
+		events[i] = hash.Event(common.HexToHash(eventIDStr))
+	}
+
+	return events
 }
