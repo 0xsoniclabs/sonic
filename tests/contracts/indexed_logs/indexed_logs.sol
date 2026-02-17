66537209edc29f7d59dae604e18491f699b5f8bb
Herbert Jordan <herbert@swrnd.ch>
2026-02-17 12:52:54 +0100

    Add integration test for log filtering

--- tests/contracts/indexed_logs/indexed_logs.sol
+++ tests/contracts/indexed_logs/indexed_logs.sol
@@ -13,4 +13,26 @@ contract IndexedLogs {
             emit Event3(i, "test string");
         }
     }
+
+    event Log1();
+    event Log2(uint256 indexed a);
+    event Log3(uint256 indexed a, uint256 indexed b);
+    event Log4(uint256 indexed a, uint256 indexed b, uint256 indexed c);
+
+    // Emits a Cartesian product of logs with up to three indexed parameters.
+    function emitCartesianProduct(uint256 n) public {
+        assembly {
+            log0(0, 0) // emit Log0() with no topics
+        }
+        emit Log1(); // log1, using the event id as topic 1
+        for (uint256 i = 0; i < n; i++) {
+            emit Log2(i);
+            for (uint256 j = 0; j < n; j++) {
+                emit Log3(i, j);
+                for (uint256 k = 0; k < n; k++) {
+                    emit Log4(i, j, k);
+                }
+            }
+        }
+    }
 }
