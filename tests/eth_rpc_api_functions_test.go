2c09304cbc119d29f95a5329b998ce1228876cdc
jenikd <jd@fantom.foundation>
2026-03-03 13:34:44 +0100

    Add simulateV1 RPC function (#672)
    * Add simulateV1 RPC function
    
    * Remove comment
    
    * Fix lint issue
    
    * Fix typo
    
    * Add simulateV1 unit tests
    
    * Refactor naming
    
    * Fix typo in test
    
    * Simplify transfer logs storage
    
    * Factor review feedback
    
    * Add tests for retrieve header
    
    * Add test for nonce validation inside tx state transition
    
    * Remove skipping of transactions to match original behavior
    
    * Replace interface{} with any
    
    * Refactor simulate code into separate files
    
    * Remove parallel test processing
    
    * Move SimulateV1 rpc function to api.go

--- tests/eth_rpc_api_functions_test.go
+++ tests/eth_rpc_api_functions_test.go
@@ -45,7 +45,6 @@ import (
 var (
 	knownMissingAPIs = namespaceMap{
 		"eth": {
-			"SimulateV1":             struct{}{},
 			"SendRawTransactionSync": struct{}{},
 			"Config":                 struct{}{}, // TODO: issue (https://github.com/0xsoniclabs/sonic-admin/issues/355)
 		},
