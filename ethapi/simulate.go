34443c2e25270251dead06d65323e1c86105f30d
jenikd <jd@fantom.foundation>
2026-03-03 11:32:37 +0100

    Refactor simulate code into separate files

--- ethapi/simulate.go
+++ ethapi/simulate.go
@@ -1,18 +1,18 @@
-// Copyright 2026 Sonic Operations Ltd
-// This file is part of the Sonic Client
+// Copyright 2024 The go-ethereum Authors
+// This file is part of the go-ethereum library.
 //
-// Sonic is free software: you can redistribute it and/or modify
+// The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Sonic is distributed in the hope that it will be useful,
+// The go-ethereum library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 package ethapi
 
@@ -52,179 +52,7 @@ const (
 
 	// timestampIncrement is the default increment between block timestamps.
 	timestampIncrement = 12
-
-	errCodeReverted                = -32000
-	errCodeVMError                 = -32015
-	errCodeInvalidParams           = -32602
-	errCodeInternalError           = -32603
-	errCodeNonceTooHigh            = -38011
-	errCodeNonceTooLow             = -38010
-	errCodeIntrinsicGas            = -38013
-	errCodeInsufficientFunds       = -38014
-	errCodeBlockGasLimitReached    = -38015
-	errCodeBlockNumberInvalid      = -38020
-	errCodeBlockTimestampInvalid   = -38021
-	errCodeSenderIsNotEOA          = -38024
-	errCodeMaxInitCodeSizeExceeded = -38025
-	errCodeClientLimitExceeded     = -38026
 )
-
-// simInvalidTxError is an error type for invalid transactions
-// in simulation, with an associated error code for JSON-RPC responses.
-type simInvalidTxError struct {
-	Message string
-	Code    int
-}
-
-func (e *simInvalidTxError) Error() string  { return e.Message }
-func (e *simInvalidTxError) ErrorCode() int { return e.Code }
-
-func simInvalidParamsError() *simInvalidTxError {
-	return &simInvalidTxError{Message: "empty input", Code: errCodeInvalidParams}
-}
-func simClientLimitExceededError() *simInvalidTxError {
-	return &simInvalidTxError{Message: "too many blocks", Code: errCodeClientLimitExceeded}
-}
-func simInvalidBlockNumberError(message string) *simInvalidTxError {
-	return &simInvalidTxError{Message: message, Code: errCodeBlockNumberInvalid}
-}
-func simInvalidBlockTimestampError(message string) *simInvalidTxError {
-	return &simInvalidTxError{Message: message, Code: errCodeBlockTimestampInvalid}
-}
-func simBlockGasLimitReachedError(message string) *simInvalidTxError {
-	return &simInvalidTxError{Message: message, Code: errCodeBlockGasLimitReached}
-}
-
-// simTxValidationError maps core transaction validation errors
-// to simInvalidTxError with appropriate message and error codes.
-func simTxValidationError(err error) *simInvalidTxError {
-	switch {
-	case errors.Is(err, core.ErrNonceTooHigh):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeNonceTooHigh}
-	case errors.Is(err, core.ErrNonceTooLow):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeNonceTooLow}
-	case errors.Is(err, core.ErrSenderNoEOA):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeSenderIsNotEOA}
-	case errors.Is(err, core.ErrFeeCapTooLow):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeInvalidParams}
-	case errors.Is(err, core.ErrTipAboveFeeCap):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeInvalidParams}
-	case errors.Is(err, core.ErrTipVeryHigh):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeInvalidParams}
-	case errors.Is(err, core.ErrFeeCapVeryHigh):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeInvalidParams}
-	case errors.Is(err, core.ErrInsufficientFunds):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeInsufficientFunds}
-	case errors.Is(err, core.ErrIntrinsicGas):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeIntrinsicGas}
-	case errors.Is(err, core.ErrMaxInitCodeSizeExceeded):
-		return &simInvalidTxError{Message: err.Error(), Code: errCodeMaxInitCodeSizeExceeded}
-	default:
-		return &simInvalidTxError{
-			Message: err.Error(),
-			Code:    errCodeInternalError,
-		}
-	}
-}
-
-var (
-	// keccak256("Transfer(address,address,uint256)")
-	simTransferTopic = common.HexToHash("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
-	// ERC-7528 canonical address for native ETH/Sonic events
-	simTransferAddress = common.HexToAddress("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
-)
-
-// simTracer collects logs for simulated transactions.
-type simTracer struct {
-	logs           []*types.Log
-	count          int
-	traceTransfers bool
-	blockNumber    uint64
-	blockTimestamp uint64
-	blockHash      common.Hash
-	txHash         common.Hash
-	txIdx          uint
-}
-
-func newSimTracer(traceTransfers bool, blockNumber, blockTimestamp uint64, blockHash, txHash common.Hash, txIndex uint) *simTracer {
-	return &simTracer{
-		traceTransfers: traceTransfers,
-		blockNumber:    blockNumber,
-		blockTimestamp: blockTimestamp,
-		blockHash:      blockHash,
-		txHash:         txHash,
-		txIdx:          txIndex,
-	}
-}
-
-// Hooks returns the tracing hooks for the simTracer.
-// They are triggered during EVM execution to capture logs.
-func (t *simTracer) Hooks() *tracing.Hooks {
-	if !t.traceTransfers {
-		return nil
-	}
-	return &tracing.Hooks{
-		OnEnter: t.onEnter,
-		OnExit:  t.onExit,
-	}
-}
-
-// onEnter is called when the EVM enters a new call frame. It captures transfer
-func (t *simTracer) onEnter(depth int, typ byte, from, to common.Address, input []byte, gas uint64, value *big.Int) {
-	if t.traceTransfers &&
-		vm.OpCode(typ) != vm.DELEGATECALL &&
-		value != nil && value.Cmp(common.Big0) > 0 {
-
-		t.captureTransfer(from, to, value)
-	}
-}
-
-// onExit is called when the EVM exits a call frame.
-func (t *simTracer) onExit(depth int, output []byte, gasUsed uint64, err error, reverted bool) {
-	if depth == 0 && reverted {
-		t.logs = nil
-		t.count = 0
-		return
-	}
-}
-
-func (t *simTracer) captureLog(address common.Address, topics []common.Hash, data []byte) {
-	t.logs = append(t.logs, &types.Log{
-		Address:        address,
-		Topics:         topics,
-		Data:           data,
-		BlockNumber:    t.blockNumber,
-		BlockTimestamp: t.blockTimestamp,
-		BlockHash:      t.blockHash,
-		TxHash:         t.txHash,
-		TxIndex:        t.txIdx,
-		Index:          uint(t.count),
-	})
-	t.count++
-}
-
-func (t *simTracer) captureTransfer(from, to common.Address, value *big.Int) {
-	if !t.traceTransfers {
-		return
-	}
-	topics := []common.Hash{
-		simTransferTopic,
-		common.BytesToHash(from.Bytes()),
-		common.BytesToHash(to.Bytes()),
-	}
-	t.captureLog(simTransferAddress, topics, common.BigToHash(value).Bytes())
-}
-
-func (t *simTracer) reset(txHash common.Hash, txIdx uint) {
-	t.logs = nil
-	t.txHash = txHash
-	t.txIdx = txIdx
-}
-
-// Logs returns the collected logs from the tracer.
-func (t *simTracer) Logs() []*types.Log {
-	return t.logs
-}
 
 // simBlockOverrides contains block header fields that can be overridden per
 // simulated block.
