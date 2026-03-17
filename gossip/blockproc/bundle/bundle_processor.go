d299e25100b6daad37a1bc7fade9e6e3fb7a0756
Lorenz Schüler <lorenz@saarow.de>
2026-03-17 09:28:42 +0100

    Add bundle processor (#732)
    Co-authored-by: Herbert <herbert@swrnd.ch>

---
+++ gossip/blockproc/bundle/bundle_processor.go
@@ -0,0 +1,91 @@
+// Copyright 2026 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package bundle
+
+import (
+	"github.com/0xsoniclabs/sonic/evmcore/core_types"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+//go:generate mockgen -source=bundle_processor.go -destination=bundle_processor_mock.go -package=bundle
+
+// RunBundle executes the transactions in the bundle using the provided
+// TransactionRunner. It returns true if the bundle execution is considered
+// successful, and false otherwise.
+//
+// This is the canonical implementation of the bundle execution logic, which
+// defines the semantic of the execution flags.
+func RunBundle(
+	bundle *TransactionBundle,
+	runner TransactionRunner,
+) bool {
+	if bundle.Flags.IsOneOf() {
+		return runOneOfBundle(bundle, runner)
+	}
+	return runAllOfBundle(bundle, runner)
+}
+
+// TransactionRunner defines an interface for running individual transactions
+// within a bundle and obtaining their results, as used by the RunBundle
+// function to determine the overall success of the bundle execution.
+type TransactionRunner interface {
+	Run(tx *types.Transaction) core_types.TransactionResult
+}
+
+// runAllOfBundle executes all transactions in the bundle and returns true if
+// all transactions are considered successful, false otherwise.
+func runAllOfBundle(
+	bundle *TransactionBundle,
+	runner TransactionRunner,
+) bool {
+	for _, tx := range bundle.Transactions {
+		result := runner.Run(tx)
+		if !isTolerated(result, bundle.Flags) {
+			return false
+		}
+	}
+	return true
+}
+
+// runOneOfBundle executes the transactions in the bundle and stops at the first
+// successful transaction. It returns true if at least one transaction is
+// considered successful, false otherwise.
+func runOneOfBundle(
+	bundle *TransactionBundle,
+	runner TransactionRunner,
+) bool {
+	for _, tx := range bundle.Transactions {
+		result := runner.Run(tx)
+		if isTolerated(result, bundle.Flags) {
+			return true
+		}
+	}
+	return false
+}
+
+func isTolerated(
+	result core_types.TransactionResult,
+	flags ExecutionFlags,
+) bool {
+	if result == core_types.TransactionResultInvalid {
+		return flags.TolerateInvalid()
+	}
+	if result == core_types.TransactionResultFailed {
+		return flags.TolerateFailed()
+	}
+	return result == core_types.TransactionResultSuccessful
+}
---
+++ gossip/blockproc/bundle/bundle_processor_test.go
@@ -0,0 +1,213 @@
+// Copyright 2026 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package bundle
+
+import (
+	"testing"
+
+	"github.com/0xsoniclabs/sonic/evmcore/core_types"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func Test_RunBundle_HandlesExecutionModeCorrectly(t *testing.T) {
+	require := require.New(t)
+	require.True(RunBundle(&TransactionBundle{Flags: EF_AllOf}, nil))
+	require.False(RunBundle(&TransactionBundle{Flags: EF_OneOf}, nil))
+}
+
+func Test_runAllOfBundle_ReturnsTrueForEmptyBundle(t *testing.T) {
+	emptyBundle := &TransactionBundle{Transactions: nil}
+	result := runAllOfBundle(emptyBundle, nil)
+	require.True(t, result)
+}
+
+func Test_runAllOfBundle_ReturnsTrueIfAllTransactionsTolerated(t *testing.T) {
+	tests := map[string]struct {
+		toleratedResult core_types.TransactionResult
+		flags           ExecutionFlags
+	}{
+		"TolerateOnlySuccessful":  {toleratedResult: core_types.TransactionResultSuccessful, flags: 0},
+		"TolerateInvalid":         {toleratedResult: core_types.TransactionResultInvalid, flags: EF_TolerateInvalid},
+		"TolerateFailed":          {toleratedResult: core_types.TransactionResultFailed, flags: EF_TolerateFailed},
+		"TolerateAll/WithInvalid": {toleratedResult: core_types.TransactionResultInvalid, flags: EF_TolerateInvalid | EF_TolerateFailed},
+		"TolerateAll/WithFailed":  {toleratedResult: core_types.TransactionResultFailed, flags: EF_TolerateInvalid | EF_TolerateFailed},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			runner := NewMockTransactionRunner(ctrl)
+
+			tx := types.NewTx(&types.LegacyTx{})
+			runner.EXPECT().Run(tx).Return(test.toleratedResult).Times(3)
+
+			bundle := &TransactionBundle{
+				Transactions: []*types.Transaction{tx, tx, tx},
+				Flags:        test.flags,
+			}
+
+			result := runAllOfBundle(bundle, runner)
+			require.True(t, result)
+		})
+	}
+}
+
+func Test_runAllOfBundle_StopsAtFirstNonToleratedTransaction(t *testing.T) {
+	tests := map[string]struct {
+		notToleratedResult core_types.TransactionResult
+		flags              ExecutionFlags
+	}{
+		"TolerateOnlySuccessful/WithInvalid": {notToleratedResult: core_types.TransactionResultInvalid, flags: 0},
+		"TolerateOnlySuccessful/WithFailed":  {notToleratedResult: core_types.TransactionResultFailed, flags: 0},
+		"TolerateInvalid":                    {notToleratedResult: core_types.TransactionResultFailed, flags: EF_TolerateInvalid},
+		"TolerateFailed":                     {notToleratedResult: core_types.TransactionResultInvalid, flags: EF_TolerateFailed},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			runner := NewMockTransactionRunner(ctrl)
+
+			tx1 := types.NewTx(&types.LegacyTx{})
+			tx2 := types.NewTx(&types.LegacyTx{})
+			tx3 := types.NewTx(&types.LegacyTx{})
+			gomock.InOrder(
+				runner.EXPECT().Run(tx1).Return(core_types.TransactionResultSuccessful),
+				runner.EXPECT().Run(tx2).Return(test.notToleratedResult),
+				// tx3 should not be run
+			)
+
+			bundle := &TransactionBundle{
+				Transactions: []*types.Transaction{tx1, tx2, tx3},
+				Flags:        test.flags,
+			}
+
+			result := runAllOfBundle(bundle, runner)
+			require.False(t, result)
+		})
+	}
+}
+
+func Test_runOneOfBundle_ReturnsFalseForEmptyBundle(t *testing.T) {
+	emptyBundle := &TransactionBundle{Transactions: nil}
+	result := runOneOfBundle(emptyBundle, nil)
+	require.False(t, result)
+}
+
+func Test_runOneOfBundle_ReturnsFalseIfAllTransactionsAreNonTolerated(t *testing.T) {
+	tests := map[string]struct {
+		notToleratedResult core_types.TransactionResult
+		flags              ExecutionFlags
+	}{
+		"TolerateOnlySuccessful/WithInvalid": {notToleratedResult: core_types.TransactionResultInvalid, flags: 0},
+		"TolerateOnlySuccessful/WithFailed":  {notToleratedResult: core_types.TransactionResultFailed, flags: 0},
+		"TolerateInvalid":                    {notToleratedResult: core_types.TransactionResultFailed, flags: EF_TolerateInvalid},
+		"TolerateFailed":                     {notToleratedResult: core_types.TransactionResultInvalid, flags: EF_TolerateFailed},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			runner := NewMockTransactionRunner(ctrl)
+
+			tx := types.NewTx(&types.LegacyTx{})
+			runner.EXPECT().Run(tx).Return(test.notToleratedResult).Times(3)
+
+			bundle := &TransactionBundle{
+				Transactions: []*types.Transaction{tx, tx, tx},
+				Flags:        test.flags,
+			}
+
+			result := runOneOfBundle(bundle, runner)
+			require.False(t, result)
+		})
+	}
+}
+
+func Test_runOneOfBundle_StopsAtFirstToleratedTransaction(t *testing.T) {
+	tests := map[string]struct {
+		notToleratedResult core_types.TransactionResult
+		flags              ExecutionFlags
+	}{
+		"TolerateOnlySuccessful/WithInvalid": {notToleratedResult: core_types.TransactionResultInvalid, flags: 0},
+		"TolerateOnlySuccessful/WithFailed":  {notToleratedResult: core_types.TransactionResultFailed, flags: 0},
+		"TolerateInvalid":                    {notToleratedResult: core_types.TransactionResultFailed, flags: EF_TolerateInvalid},
+		"TolerateFailed":                     {notToleratedResult: core_types.TransactionResultInvalid, flags: EF_TolerateFailed},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			runner := NewMockTransactionRunner(ctrl)
+
+			tx1 := types.NewTx(&types.LegacyTx{})
+			tx2 := types.NewTx(&types.LegacyTx{})
+			tx3 := types.NewTx(&types.LegacyTx{})
+
+			gomock.InOrder(
+				runner.EXPECT().Run(tx1).Return(test.notToleratedResult),
+				runner.EXPECT().Run(tx2).Return(core_types.TransactionResultSuccessful),
+				// tx3 should not be run
+			)
+
+			bundle := &TransactionBundle{
+				Transactions: []*types.Transaction{tx1, tx2, tx3},
+				Flags:        test.flags,
+			}
+
+			result := runOneOfBundle(bundle, runner)
+			require.True(t, result)
+		})
+	}
+}
+
+func Test_isTolerated_InterpretsExecutionFlagsCorrectly(t *testing.T) {
+	tests := []struct {
+		flags     ExecutionFlags
+		result    core_types.TransactionResult
+		tolerated bool
+	}{
+		{flags: 0, result: core_types.TransactionResultInvalid, tolerated: false},
+		{flags: 0, result: core_types.TransactionResultFailed, tolerated: false},
+		{flags: 0, result: core_types.TransactionResultSuccessful, tolerated: true},
+		{flags: 0, result: 99, tolerated: false}, // unknown result treated as failed
+
+		{flags: EF_TolerateInvalid, result: core_types.TransactionResultInvalid, tolerated: true},
+		{flags: EF_TolerateInvalid, result: core_types.TransactionResultFailed, tolerated: false},
+		{flags: EF_TolerateInvalid, result: core_types.TransactionResultSuccessful, tolerated: true},
+		{flags: EF_TolerateInvalid, result: 99, tolerated: false}, // unknown result treated as failed
+
+		{flags: EF_TolerateFailed, result: core_types.TransactionResultInvalid, tolerated: false},
+		{flags: EF_TolerateFailed, result: core_types.TransactionResultFailed, tolerated: true},
+		{flags: EF_TolerateFailed, result: core_types.TransactionResultSuccessful, tolerated: true},
+		{flags: EF_TolerateFailed, result: 99, tolerated: false}, // unknown result treated as failed
+
+		{flags: EF_TolerateInvalid | EF_TolerateFailed, result: core_types.TransactionResultInvalid, tolerated: true},
+		{flags: EF_TolerateInvalid | EF_TolerateFailed, result: core_types.TransactionResultFailed, tolerated: true},
+		{flags: EF_TolerateInvalid | EF_TolerateFailed, result: core_types.TransactionResultSuccessful, tolerated: true},
+		{flags: EF_TolerateInvalid | EF_TolerateFailed, result: 99, tolerated: false}, // unknown result treated as failed
+	}
+
+	for _, test := range tests {
+		require.Equal(t,
+			test.tolerated,
+			isTolerated(test.result, test.flags),
+			"flags: %b, result: %d", test.flags, test.result,
+		)
+	}
+}
