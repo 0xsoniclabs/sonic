c8d3757e81615e1a6d36e4eb97f94b865e02e186
facuMH <facumolina15@gmail.com>
2026-03-18 10:41:25 +0100

    Add execution plan store.

---
+++ gossip/store_processed_bundles.go
@@ -0,0 +1,242 @@
+// Copyright 2026 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package gossip
+
+import (
+	"encoding/binary"
+	"errors"
+
+	"github.com/0xsoniclabs/sonic/gossip/blockproc/bundle"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+const (
+	// bundleHashKeySize is the size of the key used to index processed bundle
+	// hashes by block number, which is:
+	//  - 1 byte for the prefix
+	//  - 8 bytes for the block number
+	//  - 32 bytes for the hash
+	bundleHashKeySize = 1 + 8 + 32
+
+	// bundleStateHashSize is the size of the value used to store the hash of
+	// the processed bundles history, which is:
+	//  - 8 bytes for the block number
+	//  - 32 bytes for the hash
+	bundleStateHashSize = 32 + 8
+
+	// processedBundlesHashSize is the size of the value used to store the hash of
+	// the processed bundles history, which is:
+	//  - 32 bytes for the previous hash
+	//  - 32 bytes for the added hashes
+	//  - 32 bytes for the deleted hashes
+	//  - 8 bytes for the block number
+	processedBundlesHashSize = 3*32 + 8
+)
+
+// This file implements the storage and management of processed bundles in
+// the Store. Processed bundles are tracked to prevent re-processing the same
+// bundle multiple times. The store keeps track of recently processed bundles
+// by indexing the hashes of their execution plans, along with the block number
+// and position in which they were executed. The store also maintains a hash of
+// the history of processed bundles, which is updated after every block, to
+// cross-validate that validators remain aligned on their bundle processing
+// history.
+//
+// Bundles need to be indexed by the execution plan hash instead of the hash of
+// the bundled transactions enclosing them, since otherwise the same plan may be
+// resubmitted multiple times using different envelop transactions.
+//
+// In the underlying table, the following keys are used:
+//  - key: [] -> [uint64, hash]                        // last block and hash for which the processed bundles have been stored
+//  - key: ['e']<execPlanHash> -> [block,position,count]     // for a processed bundle
+//  - key: ['i']<blockNum, execPlanHash> -> []         // for a processed bundle at a specific block number, to handle cleanups
+//
+// The hash of the processed bundle's history is computed as follows:
+//  - initially, the hash is zero
+//  - for every update, the hash is updated as follows:
+//      addedExecPlanHash = Xor(<hashes of newly added execution plans>)
+//      deletedExecPlanHash = Xor(<hashes of deleted execution plans>)
+//      newHash = Keccak256(oldHash || addedExecPlanHash || deletedExecPlanHash || blockNum)
+//
+// The hash can be used to verify that validators remain aligned on their bundle
+// processing history.
+
+// AddProcessedBundles adds the given bundle execution information for the given
+// block number. This should be called after every block, listing the bundles
+// that got accepted in the block.
+
+func (s *Store) AddProcessedBundles(blockNum uint64, executedBundles []bundle.ExecutionInfo) {
+	// Make sure there is only one update at any time.
+	s.processedBundleMutex.Lock()
+	defer s.processedBundleMutex.Unlock()
+
+	// Register and index new hashes.
+	table := s.table.ProcessedBundles
+	batch := table.NewBatch()
+	addedHash := common.Hash{}
+	for _, info := range executedBundles {
+		hash := info.ExecutionPlanHash
+
+		data := make([]byte, 16)
+		binary.BigEndian.PutUint64(data[:8], info.BlockNum)
+		binary.BigEndian.PutUint32(data[8:12], info.Position)
+		binary.BigEndian.PutUint32(data[12:], info.Count)
+
+		err := errors.Join(
+			batch.Put(getEntryKey(hash), data),
+			batch.Put(getIndexKey(blockNum, hash), []byte{0}),
+		)
+		if err != nil {
+			s.Log.Crit("failed to add processed bundle hash to batch", "error", err)
+		}
+		addedHash = xorHash(addedHash, hash)
+	}
+
+	// Delete out-dated hashes.
+	deletedHash := common.Hash{}
+	if blockNum > bundle.MaxBlockRange {
+		oldestValidBlockNum := blockNum - bundle.MaxBlockRange + 1
+		it := table.NewIterator([]byte{'i'}, nil)
+		for it.Next() {
+			key := it.Key()
+			if len(key) != bundleHashKeySize {
+				continue
+			}
+			blockNumber := binary.BigEndian.Uint64(key[1 : 1+8])
+			if blockNumber >= oldestValidBlockNum {
+				break
+			}
+			hash := common.BytesToHash(key[1+8:])
+			err := errors.Join(
+				batch.Delete(getIndexKey(blockNumber, hash)),
+				batch.Delete(getEntryKey(hash)),
+			)
+			if err != nil {
+				s.Log.Crit("failed to delete old processed bundle hash", "error", err)
+			}
+			deletedHash = xorHash(deletedHash, hash)
+		}
+	}
+
+	// Update the state hash.
+	_, oldHash := s.GetProcessedBundleHistoryHash()
+
+	update := make([]byte, processedBundlesHashSize)
+	copy(update[:32], oldHash.Bytes())
+	copy(update[32:64], addedHash.Bytes())
+	copy(update[64:96], deletedHash.Bytes())
+	binary.BigEndian.PutUint64(update[96:], blockNum)
+	newHash := common.Hash(crypto.Keccak256(update))
+
+	err := batch.Put(nil, append(
+		binary.BigEndian.AppendUint64(nil, blockNum),
+		newHash.Bytes()...,
+	))
+	if err != nil {
+		s.Log.Crit("failed to update hash of processed bundles", "error", err)
+	}
+
+	// Write all changes to the store.
+	if err := batch.Write(); err != nil {
+		s.Log.Crit("failed to create batch for processed bundles", "error", err)
+	}
+}
+
+// HasBundleRecentlyBeenProcessed checks if a bundle execution plan with the
+// given hash has been processed recently. This is used to prevent re-processing
+// the same bundle multiple times.
+//
+// Note: the store only keeps track of the bundles being executed in the last
+// bundle.MaxBlockRange blocks, so this function returns false for bundles
+// that were processed too far in the past and have been cleaned up from the
+// store.
+func (s *Store) HasBundleRecentlyBeenProcessed(execPlanHash common.Hash) bool {
+	res, err := s.table.ProcessedBundles.Get(getEntryKey(execPlanHash))
+	if err != nil {
+		s.Log.Crit("failed to check processed bundle", "error", err)
+	}
+	return res != nil
+}
+
+// GetBundleExecutionInfo returns the execution info for a processed execution
+// plan, if it is present in the store. Note that execution info is being
+// automatically removed from the store after bundle.MaxBlockRange blocks,
+// so this function returns nil for bundles that were processed too far in the
+// past.
+func (s *Store) GetBundleExecutionInfo(execPlanHash common.Hash) *bundle.ExecutionInfo {
+	res, err := s.table.ProcessedBundles.Get(getEntryKey(execPlanHash))
+	if err != nil {
+		s.Log.Crit("failed to get execution info for bundle", "error", err)
+	}
+	if res == nil {
+		return nil
+	}
+	if len(res) != 16 {
+		s.Log.Crit("invalid data length for execution info", "length", len(res))
+	}
+	blockNum := binary.BigEndian.Uint64(res[:8])
+	startPosition := binary.BigEndian.Uint32(res[8:12])
+	count := binary.BigEndian.Uint32(res[12:])
+	return &bundle.ExecutionInfo{
+		ExecutionPlanHash: execPlanHash,
+		BlockNum:          blockNum,
+		Position:          startPosition,
+		Count:             count,
+	}
+}
+
+// GetProcessedBundleHistoryHash returns the current hash of the processed
+// bundles history, along with the block number of the last update.
+func (s *Store) GetProcessedBundleHistoryHash() (uint64, common.Hash) {
+	state, err := s.table.ProcessedBundles.Get(nil)
+	if err != nil {
+		s.Log.Crit("failed to get hash of processed bundles", "error", err)
+	}
+	if state == nil {
+		return 0, common.Hash{}
+	}
+	if len(state) != bundleStateHashSize {
+		s.Log.Crit("invalid state length for processed bundles", "length", len(state))
+	}
+	blockNum := binary.BigEndian.Uint64(state[:8])
+	hash := common.BytesToHash(state[8:])
+	return blockNum, hash
+}
+
+// --- utility functions for processed bundles management ---
+
+// getEntryKey returns the key used to store the presence of a processed bundle
+// hash.
+func getEntryKey(hash common.Hash) []byte {
+	return append([]byte{'e'}, hash.Bytes()...)
+}
+
+// getIndexKey returns the key used to index a processed bundle hash at a
+// specific block number, to handle cleanups.
+func getIndexKey(blockNum uint64, hash common.Hash) []byte {
+	return append(append([]byte{'i'}, binary.BigEndian.AppendUint64(nil, blockNum)...), hash.Bytes()...)
+}
+
+// xorHash returns the XOR of two hashes.
+func xorHash(a, b common.Hash) common.Hash {
+	var res common.Hash
+	for i := 0; i < len(res); i++ {
+		res[i] = a[i] ^ b[i]
+	}
+	return res
+}
