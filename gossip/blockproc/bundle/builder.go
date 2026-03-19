d6ab158516f0b63ac53f67d75581ff7af563d5e1
Herbert <herbert@swrnd.ch>
2026-03-19 16:30:51 +0100

    Add bundle builder (#736)
    Co-authored-by: Luis Ayuso <luis@ph3.at>
    Co-authored-by: facuMH <facumolina15@gmail.com>
    Co-authored-by: Lorenz Schüler <lorenz@saarow.de>

---
+++ gossip/blockproc/bundle/builder.go
@@ -0,0 +1,326 @@
+// Copyright 2026 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package bundle
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// This file offers utilities to build bundles from transaction data. The most
+// generic format is the NewBundle function, enabling the creation of an
+// envelope transaction carrying a bundle as follows:
+//
+//   envelope := NewBuilder(signer).
+// 		SetFlags(EF_AllOf|EF_TolerateFailed).
+// 		SetEarliest(12).
+// 		SetLatest(15).
+// 		With(
+// 			Step(key, &types.AccessListTx{
+// 				Nonce: 1,
+// 			}),
+// 			Step(key, &types.AccessListTx{
+// 				Nonce: 2,
+// 			}),
+// 		).
+//  	Build()
+//
+// The resulting envelope carries a valid bundle of signed transactions.
+// For convenience, further abbreviations are supported. For example:
+//
+//    envelopeA := AllOf(
+// 			signer,
+// 			Step(key, &types.AccessListTx{
+// 				Nonce: 1,
+// 			}),
+// 			Step(key, &types.AccessListTx{
+// 				Nonce: 2,
+// 			}),
+//    )
+//
+// Also nested bundles are supported by using
+//
+//    envelopeB := OneOf(
+// 			signer,
+// 			Step(key, envelopeA),
+// 			Step(key, AllOf(
+// 				Step(key, &types.AccessListTx{
+// 					Nonce: 1,
+// 				}),
+// 				Step(key, &types.AccessListTx{
+// 					Nonce: 2,
+// 				}),
+// 			)),
+//    )
+//
+// The hope for this library is to provide means for the readable generation of
+// bundles in unit tests.
+
+// Step creates a transaction to be included in a bundle, signed by the given
+// key. It is a building block to be used as an argument in the builder or in
+// utility functions.
+func Step(key *ecdsa.PrivateKey, tx any) BundleStep {
+	switch tx := tx.(type) {
+	case types.TxData:
+		return BundleStep{key: key, tx: tx}
+	case types.AccessListTx:
+		return BundleStep{key: key, tx: &tx}
+	case types.DynamicFeeTx:
+		return BundleStep{key: key, tx: &tx}
+	case types.BlobTx:
+		return BundleStep{key: key, tx: &tx}
+	case types.SetCodeTx:
+		return BundleStep{key: key, tx: &tx}
+	case *types.Transaction:
+
+		if tx.Type() != types.AccessListTxType &&
+			tx.Type() != types.LegacyTxType {
+			// this code path is used to nest bundles, if
+			// you need any other transaction type, please add it
+			//
+			// not doing this check will lead to data loss
+			panic(" unsupported Tx type for Step. Only AccessListTx and LegacyTx are supported")
+		}
+
+		return Step(key, &types.AccessListTx{
+			ChainID:    tx.ChainId(),
+			Nonce:      tx.Nonce(),
+			GasPrice:   tx.GasPrice(),
+			Gas:        tx.Gas(),
+			To:         tx.To(),
+			Value:      tx.Value(),
+			Data:       tx.Data(),
+			AccessList: tx.AccessList(),
+		})
+	default:
+		panic("unsupported TxData type")
+	}
+}
+
+// BundleStep is a single transaction in a bundle to build.
+type BundleStep struct {
+	key *ecdsa.PrivateKey
+	tx  types.TxData
+}
+
+// NewBuilder creates a new bundle builder to create a custom bundle.
+func NewBuilder(signer types.Signer) *builder {
+	return &builder{signer: signer}
+}
+
+type builder struct {
+	signer      types.Signer
+	flags       *ExecutionFlags
+	earliest    *uint64
+	latest      *uint64
+	steps       []BundleStep
+	envelopeKey *ecdsa.PrivateKey
+}
+
+func (b *builder) SetFlags(flags ExecutionFlags) *builder {
+	b.flags = &flags
+	return b
+}
+
+func (b *builder) SetEarliest(earliest uint64) *builder {
+	b.earliest = &earliest
+	return b
+}
+
+func (b *builder) SetLatest(latest uint64) *builder {
+	b.latest = &latest
+	return b
+}
+
+func (b *builder) With(steps ...BundleStep) *builder {
+	b.steps = append(b.steps, steps...)
+	return b
+}
+
+func (b *builder) SetEnvelopeSenderKey(key *ecdsa.PrivateKey) *builder {
+	b.envelopeKey = key
+	return b
+}
+
+func (b *builder) BuildBundleAndPlan() (*TransactionBundle, ExecutionPlan) {
+
+	// Set up defaults for meta flags.
+	flags := EF_AllOf
+	if b.flags != nil {
+		flags = *b.flags
+	}
+	earliest := uint64(0)
+	latest := uint64(MaxBlockRange - 1)
+	if b.earliest != nil {
+		earliest = *b.earliest
+		latest = earliest + MaxBlockRange - 1
+	}
+	if b.latest != nil {
+		latest = *b.latest
+	}
+
+	signer := b.signer
+	if signer == nil {
+		signer = types.LatestSignerForChainID(big.NewInt(1))
+	}
+
+	// Create an Execution Plan for the bundle.
+
+	plan := ExecutionPlan{
+		Steps:    make([]ExecutionStep, len(b.steps)),
+		Flags:    flags,
+		Earliest: earliest,
+		Latest:   latest,
+	}
+	for i, step := range b.steps {
+		plan.Steps[i] = ExecutionStep{
+			From: crypto.PubkeyToAddress(step.key.PublicKey),
+			Hash: signer.Hash(types.NewTx(step.tx)),
+		}
+	}
+
+	// Get hash of execution plan and annotate transactions with it.
+	execPlanHash := plan.Hash()
+	marker := types.AccessTuple{
+		Address:     BundleOnly,
+		StorageKeys: []common.Hash{execPlanHash},
+	}
+	for _, step := range b.steps {
+		switch data := step.tx.(type) {
+		case *types.DynamicFeeTx:
+			data.AccessList = append(data.AccessList, marker)
+		case *types.AccessListTx:
+			data.AccessList = append(data.AccessList, marker)
+		case *types.BlobTx:
+			data.AccessList = append(data.AccessList, marker)
+		case *types.SetCodeTx:
+			data.AccessList = append(data.AccessList, marker)
+		default:
+			panic("unsupported TxData type for marker annotation")
+		}
+	}
+
+	// Sign the modified TxData instances.
+	txs := make([]*types.Transaction, len(b.steps))
+	for i, step := range b.steps {
+		txs[i] = types.MustSignNewTx(step.key, signer, step.tx)
+	}
+
+	return &TransactionBundle{
+		Transactions: txs,
+		Flags:        flags,
+		Earliest:     earliest,
+		Latest:       latest,
+	}, plan
+}
+
+// BuildEnvelopeBundleAndPlan returns an envelope transaction along its
+// bundle and execution plan
+func (b *builder) BuildEnvelopeBundleAndPlan() (
+	*types.Transaction,
+	*TransactionBundle,
+	ExecutionPlan,
+) {
+	// Build the bundle and wrap it in an envelope.
+	key := b.envelopeKey
+	if key == nil {
+		newKey, err := crypto.GenerateKey()
+		if err != nil {
+			panic(fmt.Sprintf("failed to generate new key: %v", err))
+		}
+		key = newKey
+	}
+	bundle, plan := b.BuildBundleAndPlan()
+	return newEnvelope(key, bundle), bundle, plan
+}
+
+// BuildEnvelope returns an envelope transaction and its execution plan
+func (b *builder) BuildEnvelopeAndPlan() (*types.Transaction, ExecutionPlan) {
+	envelop, _, plan := b.BuildEnvelopeBundleAndPlan()
+	return envelop, plan
+}
+
+// Build returns an envelope transaction
+func (b *builder) Build() *types.Transaction {
+	envelope, _ := b.BuildEnvelopeAndPlan()
+	return envelope
+}
+
+// --- Utility Wrappers ---
+
+func AllOf(signer types.Signer, steps ...BundleStep) *types.Transaction {
+	return NewBuilder(signer).SetFlags(EF_AllOf).With(steps...).Build()
+}
+
+func OneOf(signer types.Signer, steps ...BundleStep) *types.Transaction {
+	return NewBuilder(signer).SetFlags(EF_OneOf).With(steps...).Build()
+}
+
+// --- implementation details ---
+
+// Wraps the given bundle into an envelope transaction.
+func newEnvelope(
+	key *ecdsa.PrivateKey,
+	bundle *TransactionBundle,
+) *types.Transaction {
+
+	payload := bundle.Encode()
+
+	intrinsic, err := core.IntrinsicGas(
+		payload,
+		nil,   // access list is not used in the envelope transaction
+		nil,   // code auth is not used in the bundle transaction
+		false, // bundle transaction is not a contract creation
+		true,  // is homestead
+		true,  // is istanbul
+		true,  // is shanghai
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	floorDataGas, err := core.FloorDataGas(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	txGasSum := uint64(0)
+	for _, tx := range bundle.Transactions {
+		txGasSum += tx.Gas()
+	}
+
+	gasLimit := max(intrinsic, floorDataGas, txGasSum)
+
+	chainId := big.NewInt(1)
+	if len(bundle.Transactions) > 0 {
+		chainId = bundle.Transactions[0].ChainId()
+	}
+
+	signer := types.LatestSignerForChainID(chainId)
+	return types.MustSignNewTx(key, signer, &types.AccessListTx{
+		ChainID: chainId,
+		To:      &BundleProcessor,
+		Data:    payload,
+		Gas:     gasLimit,
+	})
+}
---
+++ gossip/blockproc/bundle/builder_test.go
@@ -0,0 +1,231 @@
+// Copyright 2026 Sonic Operations Ltd
+// This file is part of the Sonic Client
+//
+// Sonic is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Sonic is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
+
+package bundle
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBundleBuilder_Build_AllowsToBuildBundleAsSpecified(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+
+	key1, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	key2, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	keyE, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	tx := NewBuilder(signer).
+		SetFlags(EF_AllOf|EF_TolerateFailed).
+		SetEarliest(12).
+		SetLatest(15).
+		With(
+			Step(key1, &types.AccessListTx{
+				Nonce: 1,
+			}),
+			Step(key2, &types.AccessListTx{
+				Nonce: 2,
+			}),
+		).
+		SetEnvelopeSenderKey(keyE).
+		Build()
+
+	bundle, plan, err := ValidateEnvelope(signer, tx)
+	require.NoError(t, err)
+
+	require.Equal(t, EF_AllOf|EF_TolerateFailed, plan.Flags)
+	require.EqualValues(t, 12, plan.Earliest)
+	require.EqualValues(t, 15, plan.Latest)
+
+	txs := bundle.Transactions
+	require.Len(t, txs, 2)
+
+	sender1, err := signer.Sender(txs[0])
+	require.NoError(t, err)
+	require.Equal(t, crypto.PubkeyToAddress(key1.PublicKey), sender1)
+
+	sender2, err := signer.Sender(txs[1])
+	require.NoError(t, err)
+	require.Equal(t, crypto.PubkeyToAddress(key2.PublicKey), sender2)
+
+	envelopeSigner, err := signer.Sender(tx)
+	require.NoError(t, err)
+	require.Equal(t, crypto.PubkeyToAddress(keyE.PublicKey), envelopeSigner)
+}
+
+func TestBundleBuilder_Step_AcceptsVariousInputTypes(t *testing.T) {
+	inputs := []any{
+		types.AccessListTx{},
+		types.DynamicFeeTx{},
+		types.BlobTx{},
+		types.SetCodeTx{},
+		&types.AccessListTx{},
+		&types.DynamicFeeTx{},
+		&types.BlobTx{},
+		&types.SetCodeTx{},
+		types.NewTx(&types.LegacyTx{}), // = *types.Transaction
+	}
+
+	for _, input := range inputs {
+		require.NotPanics(t, func() {
+			Step(nil, input)
+		})
+	}
+}
+
+func TestBundleBuilder_Panics_WhenNestingUnsupportedTxTypes(t *testing.T) {
+
+	cases := []types.TxData{
+		&types.DynamicFeeTx{},
+		&types.BlobTx{},
+		&types.SetCodeTx{},
+	}
+
+	for _, txData := range cases {
+		tx := types.NewTx(txData)
+		t.Run(fmt.Sprintf("TxType%d", tx.Type()), func(t *testing.T) {
+
+			require.Panics(t, func() {
+				Step(nil, tx)
+			}, "unsupported Tx type for Step. Only AccessListTx and LegacyTx are supported")
+		})
+	}
+}
+
+func TestBundleBuilder_Step_PanicsOnInvalidInput(t *testing.T) {
+	require.Panics(t, func() {
+		Step(nil, 12)
+	}, "unsupported TxData type")
+}
+
+func TestBundleBuilder_AllOf_BuildEmptyBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+	tx := AllOf(signer)
+
+	_, _, err := ValidateEnvelope(signer, tx)
+	require.NoError(t, err)
+}
+
+func TestBundleBuilder_AllOf_BuildBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+
+	key, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	tx := AllOf(signer,
+		Step(key, &types.AccessListTx{
+			Nonce: 0,
+		}),
+		Step(key, &types.DynamicFeeTx{
+			Nonce: 1,
+		}),
+		Step(key, &types.AccessListTx{
+			Nonce: 2,
+		}),
+	)
+
+	_, _, err = ValidateEnvelope(signer, tx)
+	require.NoError(t, err)
+}
+
+func TestBundleBuilder_OneOf_BuildBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+
+	key, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	tx := OneOf(signer,
+		Step(key, &types.AccessListTx{
+			Nonce: 0,
+		}),
+		Step(key, &types.DynamicFeeTx{
+			Nonce: 1,
+		}),
+		Step(key, &types.AccessListTx{
+			Nonce: 2,
+		}),
+	)
+
+	_, _, err = ValidateEnvelope(signer, tx)
+	require.NoError(t, err)
+}
+
+func TestBundleBuilder_OneOf_EmptyBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+	tx := OneOf(signer)
+
+	_, _, err := ValidateEnvelope(signer, tx)
+	require.NoError(t, err)
+}
+
+func TestBundleBuilder_Builder_NewNestedBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(testChainID)
+
+	key, err := crypto.GenerateKey()
+	require.NoError(t, err)
+
+	inner := OneOf(signer,
+		Step(key, &types.AccessListTx{
+			Nonce: 0,
+		}),
+		Step(key, &types.DynamicFeeTx{
+			Nonce: 1,
+		}),
+		Step(key, &types.AccessListTx{
+			Nonce: 2,
+		}),
+	)
+
+	outer := AllOf(signer,
+		Step(key, &types.AccessListTx{
+			Nonce: 2,
+		}),
+		Step(key, inner),
+		Step(key, &types.AccessListTx{
+			Nonce: 3,
+		}),
+	)
+
+	_, _, err = ValidateEnvelope(signer, inner)
+	require.NoError(t, err)
+
+	_, _, err = ValidateEnvelope(signer, outer)
+	require.NoError(t, err)
+
+	// all combined in one
+
+	combined := AllOf(signer,
+		Step(key, OneOf(signer,
+			Step(key, &types.AccessListTx{}),
+			Step(key, &types.DynamicFeeTx{}),
+		)),
+		Step(key, AllOf(signer,
+			Step(key, &types.AccessListTx{}),
+		)),
+	)
+
+	_, _, err = ValidateEnvelope(signer, combined)
+	require.NoError(t, err)
+}
