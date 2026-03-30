0aeffad9bc47043f2cf6d0f83e8696635864e5f2
Luis Ayuso <luis@ph3.at>
2026-03-30 10:14:45 +0200

    WIP

--- api/rpc_test/state.go
+++ api/rpc_test/state.go
@@ -16,6 +16,8 @@ type testState struct {
 
 func newTestState() *testState {
 	carmenDir, err := os.MkdirTemp("", "test.carmen")
+	// TODO: pass testing.T and use t.TempDir() instead of os.MkdirTemp,
+	// which will automatically clean up the directory after the test finishes.
 	if err != nil {
 		panic(fmt.Errorf("failed to create temporary dir for GenesisBuilder: %v", err))
 	}
@@ -36,28 +38,10 @@ func newTestState() *testState {
 }
 
 func (t *testState) Copy() state.StateDB {
-
 	// FIXME: do copy
-
-	carmenDir, err := os.MkdirTemp("", "test.carmen")
-	if err != nil {
-		panic(fmt.Errorf("failed to create temporary dir for GenesisBuilder: %v", err))
-	}
-	carmenState, err := carmen.NewState(carmen.Parameters{
-		Variant:      "go-file",
-		Schema:       carmen.Schema(5),
-		Archive:      carmen.S5Archive,
-		Directory:    carmenDir,
-		LiveCache:    1, // use minimum cache (not default)
-		ArchiveCache: 1, // use minimum cache (not default)
-	})
-	if err != nil {
-		panic(fmt.Errorf("failed to create carmen state; %s", err))
-	}
-	// Set cache size to lowest value possible
-	carmenStateDb := carmen.CreateCustomStateDBUsing(carmenState, 1024)
-	return &testState{evmstore.CreateCarmenStateDb(carmenStateDb)}
+	panic("not implemented")
 }
 
 func (t *testState) setAccount(addr common.Address, acc TestAccount) {
+	panic("not implemented")
 }
