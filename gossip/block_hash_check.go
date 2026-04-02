d72bf0aa99c138cf3f72d495e674ae0df24e5e79
Bernhard Scholz <14209113+b-scholz@users.noreply.github.com>
2026-04-02 14:42:34 +1100

    Fix header

--- gossip/block_hash_check.go
+++ gossip/block_hash_check.go
@@ -14,22 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Sonic. If not, see <http://www.gnu.org/licenses/>.
 
-// Copyright 2024 The Sonic Authors
-// This file is part of the Sonic library.
-//
-// Sonic is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Sonic is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Sonic. If not, see <http://www.gnu.org/licenses/>.
-
 package gossip
 
 import (
