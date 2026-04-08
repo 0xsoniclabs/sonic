3f0137568c1227e8392098ee5c6dab0564a841b8
Herbert Jordan <herbert@swrnd.ch>
2026-04-08 08:49:28 +0200

    Integrate bundle emission into emitter

--- gossip/emitter/txs_test.go
+++ gossip/emitter/txs_test.go
@@ -17,10 +17,21 @@
 package emitter
 
 import (
+	"fmt"
+	"math/big"
 	"testing"
 
+	"github.com/0xsoniclabs/sonic/evmcore"
+	"github.com/0xsoniclabs/sonic/gossip/blockproc/bundle"
 	"github.com/0xsoniclabs/sonic/gossip/emitter/config"
+	"github.com/0xsoniclabs/sonic/inter"
+	"github.com/0xsoniclabs/sonic/inter/state"
+	"github.com/0xsoniclabs/sonic/opera"
+	"github.com/Fantom-foundation/lachesis-base/inter/idx"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func Test_DefaultMaxTxsPerAddress_Equals_txTurnNonces(t *testing.T) {
@@ -32,4 +43,208 @@ func Test_DefaultMaxTxsPerAddress_Equals_txTurnNonces(t *testing.T) {
 
 	defaultConfig := config.DefaultConfig()
 	require.EqualValues(t, txTurnNonces, defaultConfig.MaxTxsPerAddress, "Default MaxTxsPerAddress should equal txTurnNonces")
+}
+
+func Test_Emitter_isValidBundleTx_AcceptsValidBundleIfBundlesAreEnabled(t *testing.T) {
+	for _, bundlesEnabled := range []bool{true, false} {
+		t.Run(fmt.Sprintf("enabled=%t", bundlesEnabled), func(t *testing.T) {
+			require := require.New(t)
+			ctrl := gomock.NewController(t)
+
+			rules := opera.Rules{
+				NetworkID: 12,
+				Upgrades: opera.Upgrades{
+					TransactionBundles: bundlesEnabled,
+				},
+			}
+
+			state := state.NewMockStateDB(ctrl)
+			state.EXPECT().HasBundleRecentlyBeenProcessed(gomock.Any()).AnyTimes()
+
+			external := NewMockExternal(ctrl)
+			external.EXPECT().GetRules().Return(rules).AnyTimes()
+			external.EXPECT().GetLatestBlockIndex().Return(idx.Block(100)).AnyTimes()
+			external.EXPECT().StateDB().Return(state).AnyTimes()
+
+			emitter := &Emitter{
+				world: World{External: external},
+			}
+
+			signer := types.LatestSignerForChainID(big.NewInt(int64(rules.NetworkID)))
+			tx := bundle.NewBuilder(signer).SetEarliest(50).SetLatest(150).Build()
+
+			_, _, err := bundle.ValidateEnvelope(signer, tx)
+			require.NoError(err)
+
+			allBundlesRunnable := func(evmcore.ChainState, *types.Transaction) evmcore.BundleState {
+				return evmcore.BundleState{Executable: true}
+			}
+
+			require.Equal(bundlesEnabled, emitter.isRunnableBundleTxInternal(tx, allBundlesRunnable))
+		})
+	}
+}
+
+func Test_Emitter_isValidBundleTx_RejectsInvalidBundle(t *testing.T) {
+	signer := types.LatestSignerForChainID(big.NewInt(1))
+	tests := map[string]*types.Transaction{
+		"not a bundle": types.NewTx(&types.LegacyTx{}),
+		"invalid bundle data": types.NewTx(&types.LegacyTx{
+			To:   &bundle.BundleProcessor,
+			Data: []byte{0x01, 0x02, 0x03},
+		}),
+		"bundle with out-of-range block numbers": bundle.NewBuilder(signer).
+			SetEarliest(150).
+			SetLatest(250).
+			Build(),
+	}
+
+	for name, tx := range tests {
+		t.Run(name, func(t *testing.T) {
+			require := require.New(t)
+			ctrl := gomock.NewController(t)
+
+			rules := opera.Rules{
+				Upgrades: opera.Upgrades{
+					TransactionBundles: true,
+				},
+			}
+
+			state := state.NewMockStateDB(ctrl)
+			state.EXPECT().HasBundleRecentlyBeenProcessed(gomock.Any()).AnyTimes()
+
+			external := NewMockExternal(ctrl)
+			external.EXPECT().GetRules().Return(rules).AnyTimes()
+			external.EXPECT().GetLatestBlockIndex().Return(idx.Block(100)).AnyTimes()
+			external.EXPECT().StateDB().Return(state).AnyTimes()
+
+			emitter := &Emitter{
+				world: World{External: external},
+			}
+
+			require.False(emitter.isValidBundleTx(tx))
+		})
+	}
+}
+
+func Test_Emitter_isValidBundleTx_RejectsAlreadyProcessedBundle(t *testing.T) {
+	for _, processed := range []bool{true, false} {
+		t.Run(fmt.Sprintf("processed=%t", processed), func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			rules := opera.Rules{
+				Upgrades: opera.Upgrades{
+					TransactionBundles: true,
+				},
+			}
+
+			state := state.NewMockStateDB(ctrl)
+			state.EXPECT().HasBundleRecentlyBeenProcessed(gomock.Any()).Return(processed).AnyTimes()
+
+			external := NewMockExternal(ctrl)
+			external.EXPECT().GetRules().Return(rules).AnyTimes()
+			external.EXPECT().GetLatestBlockIndex().Return(idx.Block(100)).AnyTimes()
+			external.EXPECT().StateDB().Return(state).AnyTimes()
+
+			emitter := &Emitter{
+				world: World{External: external},
+			}
+
+			signer := types.LatestSignerForChainID(big.NewInt(1))
+			tx := bundle.NewBuilder(signer).SetEarliest(50).SetLatest(150).Build()
+
+			_, _, err := bundle.ValidateEnvelope(signer, tx)
+			require.NoError(t, err)
+
+			getBundleState := func(evmcore.ChainState, *types.Transaction) evmcore.BundleState {
+				return evmcore.BundleState{Executable: true}
+			}
+
+			require.Equal(t, !processed, emitter.isRunnableBundleTxInternal(tx, getBundleState))
+		})
+	}
+}
+
+func Test_preCheckStateAdapter_ForwardsNetworkRuleRequest(t *testing.T) {
+	rules := opera.Rules{
+		NetworkID: 42,
+	}
+
+	ctrl := gomock.NewController(t)
+	external := NewMockExternal(ctrl)
+	external.EXPECT().GetRules().Return(rules)
+
+	adapter := &preCheckChainStateAdapter{external: external}
+	returnedRules := adapter.GetCurrentNetworkRules()
+
+	require.Equal(t, rules, returnedRules)
+}
+
+func Test_preCheckStateAdapter_ForwardsStateDBRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	stateDB := state.NewMockStateDB(ctrl)
+	stateDB.EXPECT().HasBundleRecentlyBeenProcessed(gomock.Any()).AnyTimes()
+
+	external := NewMockExternal(ctrl)
+	external.EXPECT().StateDB().Return(stateDB)
+
+	adapter := &preCheckChainStateAdapter{external: external}
+	returnedStateDB := adapter.StateDB()
+
+	require.Same(t, stateDB, returnedStateDB)
+}
+
+func Test_preCheckStateAdapter_ForwardsHeaderRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	header := &evmcore.EvmHeader{}
+
+	hash := common.Hash{1, 2, 3}
+	number := uint64(42)
+
+	external := NewMockExternal(ctrl)
+	external.EXPECT().Header(hash, number).Return(header)
+
+	adapter := &preCheckChainStateAdapter{external: external}
+	returnedHeader := adapter.Header(hash, number)
+
+	require.Same(t, header, returnedHeader)
+}
+
+func Test_preCheckStateAdapter_UsesNetworkRulesAndUpgradeHeights(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	rules := opera.Rules{NetworkID: 42}
+
+	heights := []opera.UpgradeHeight{
+		{Height: 100, Upgrades: opera.Upgrades{Sonic: true}},
+		{Height: 200, Upgrades: opera.Upgrades{Allegro: true}},
+	}
+
+	blockHeight := idx.Block(150)
+
+	external := NewMockExternal(ctrl)
+	external.EXPECT().GetRules().Return(rules)
+	external.EXPECT().GetUpgradeHeights().Return(heights)
+
+	adapter := &preCheckChainStateAdapter{external: external}
+	got := adapter.GetEvmChainConfig(blockHeight)
+
+	expected := opera.CreateTransientEvmChainConfig(rules.NetworkID, heights, blockHeight)
+	require.Equal(t, expected, got)
+}
+
+func Test_preCheckStateAdapter_ForwardsGetLatestHeader(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	header := &evmcore.EvmHeader{}
+
+	block := inter.Block{}
+	block.Number = 42
+
+	external := NewMockExternal(ctrl)
+	external.EXPECT().GetLatestBlock().Return(&block)
+	external.EXPECT().Header(block.Hash(), block.Number).Return(header)
+
+	adapter := &preCheckChainStateAdapter{external: external}
+	returnedHeader := adapter.GetLatestHeader()
+
+	require.Same(t, header, returnedHeader)
 }
