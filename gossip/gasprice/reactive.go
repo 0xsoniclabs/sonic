43588688adfcbc2903d9810c6ce1814fa5a87ae6
Simon <simon@fantom.foundation>
2026-04-23 10:04:07 +0200

    gas tip computation

--- gossip/gasprice/reactive.go
+++ gossip/gasprice/reactive.go
@@ -264,7 +264,7 @@ func (gpo *Oracle) calcTxpoolStat() txpoolStat {
 	for _, tx := range sorted {
 		for p < uint64(len(s.percentiles)) && gasCounter >= p*maxGasToIndex/uint64(len(s.percentiles)) {
 			// EffectiveGasTip returns an error for negative values, this is no problem here
-			gasTip, _ := tx.EffectiveGasTip(minGasPrice)
+			gasTip, _ := EffectiveGasTip(tx, minGasPrice)
 			s.percentiles[p] = gasTip
 			if s.percentiles[p].Sign() < 0 {
 				s.percentiles[p] = minGasPrice
