b99640a0a32deac717ed0287c01c05073622ceab
Luis Ayuso <luis@ph3.at>
2026-04-28 15:08:28 +0200

    Add bundles rpc test combining both prepare and submit.

--- api/sonicapi/execution_plan.go
+++ api/sonicapi/execution_plan.go
@@ -156,7 +156,7 @@ func toBundleExecutionGroup(l RPCExecutionPlanGroup) (bundle.ExecutionStep, erro
 		steps[i] = step
 	}
 
-	// if it's not a group, skip it
+	// Single children without flags do not need to be in an extra group
 	if !l.TolerateFailures && len(steps) == 1 {
 		return steps[0], nil
 	}
