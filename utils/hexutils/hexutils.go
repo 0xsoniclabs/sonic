93bea639bf996c0a2e6177e41e2634051a620342
facuMH <facumolina15@gmail.com>
2026-05-26 16:42:59 +0200

    Replace regex by strings.Replace.

--- utils/hexutils/hexutils.go
+++ utils/hexutils/hexutils.go
@@ -20,13 +20,13 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
-	"regexp"
+	"strings"
 )
 
 // HexToBytes converts a hex string to a byte sequence.
 // The hex string can have spaces between bytes.
 func HexToBytes(s string) []byte {
-	s = regexp.MustCompile(" ").ReplaceAllString(s, "")
+	s = strings.ReplaceAll(s, " ", "")
 	b := make([]byte, hex.DecodedLen(len(s)))
 	_, err := hex.Decode(b, []byte(s))
 	if err != nil {
